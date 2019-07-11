7f6b4a32876484377f1b3cae53e0b5bb7a48946d
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2019-07-11 01:23:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.3 MB (52.88%)

---
+++ 0742-to-lower-case/solution.cpp
@@ -0,0 +1,16 @@
+#include <cctype>
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        
+        
+        for(int i = 0; i < str.length(); i++)
+        {
+            str[i] = tolower(str[i]);
+        }
+        
+        return str;
+        
+    }
+};
