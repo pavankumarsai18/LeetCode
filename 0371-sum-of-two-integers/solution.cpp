0d02818a067ff2757d4172014798479cdee75545
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2019-12-22 15:20:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (2.36%)

---
+++ 0371-sum-of-two-integers/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum = 0;
+        unsigned int carry = 0;
+        for (int i = 0; i < 32; i++) {
+            int A = a & (1 << i);
+            int B = b & (1 << i);
+            sum |= A ^ B ^ carry;
+            carry = ((A & B) | (B & carry) | (A & carry)) << 1;
+        }
+        
+        return sum;
+        
+    }
+};
