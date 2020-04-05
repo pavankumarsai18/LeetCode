03a91d661f632988cdcbd5ec014ad265acdbe985
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2020-04-05 01:22:36 +0000

    [LeetCode Sync] Runtime - 4 ms (7.21%), Memory - 6.2 MB (100.00%)

--- 0202-happy-number/solution.cpp
+++ 0202-happy-number/solution.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
-    bool isHappy(int n) 
-    {
-        //a^2 + b^2
+    bool isHappy(int n) {
         
-        int temp;
-        int count = 0;
-        int x = n;
-        
-        unordered_set<int> cycle;
-        cycle.insert(n);
-        do
+        unordered_set<int> S;
+        while(n != 1)
         {
-            vector<int>num;
-            while(x != 0)
-            {
-                num.push_back(x%10);
-                x/=10;
-            }
-            temp = 0;
-            for(int i = 0; i < num.size(); i++)
-            {
-                temp += num[i]*num[i];
-            }
-            if(temp == 1)
-                return true;
-            count++;
-            x = temp;
-            auto itr = cycle.find(x);
-            if(itr == cycle.end())
-            {
-                cycle.insert(x);
-            }
+            auto itr = S.find(n);
+            if(itr == S.end())
+                S.insert(n);
             else
-            {
                 return false;
+            int temp = n;
+            int res = 0;
+            while(temp != 0)
+            {
+                int digit = temp%10;
+                res += digit*digit;
+                temp /= 10;
             }
+            n = res;
         }
-        while(temp != 1 && count != n);
-        
-        return false;
+        return true;
         
     }
 };
