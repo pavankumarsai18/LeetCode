dfb4d387878665c127116b3f460137280bab5ac3
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2020-12-16 19:35:11 +0000

    [LeetCode Sync] Runtime - 68 ms (5.01%), Memory - 27.1 MB (100.00%)

--- 1019-squares-of-a-sorted-array/solution.cpp
+++ 1019-squares-of-a-sorted-array/solution.cpp
@@ -1,14 +1,50 @@
-#include <algorithm>
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& A) {
+    vector<int> sortedSquares(vector<int>& nums) {
         
-        for(int i = 0; i < A.size(); i++)
+        // O(n) two pointer
+        
+        int startIndex = 0;
+        int endIndex = nums.size() - 1;
+        vector<int> result;
+        
+        while(startIndex <= endIndex)
         {
-            A[i] *= A[i];
+            int first = nums[startIndex]*nums[startIndex];
+            int last = nums[endIndex]*nums[endIndex];
+            
+            if(first > last)
+            {
+                result.push_back(first);
+                startIndex++;
+            }
+            else if(last > first)
+            {
+                result.push_back(last);
+                endIndex--;
+            }
+            else
+            {
+                if(startIndex == endIndex){
+                     result.push_back(last);
+                }
+                else{
+                    result.push_back(first);
+                    result.push_back(last);
+                }
+                startIndex++;
+                endIndex--;
+            }
         }
         
-        std::sort(A.begin(), A.end());
-        return A;
+        for(size_t i = 0; i < result.size()/2; i++)
+        {
+            int temp = result[i];
+            result[i] = result[result.size() - 1 - i];
+            result[result.size() - 1 - i] = temp;
+        }
+        return result;
+        
+        
     }
 };
