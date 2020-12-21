9bbbb731a683ce8336a8983197e5630cd1824a01
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2020-12-21 00:11:18 +0000

    [LeetCode Sync] Runtime - 4 ms (5.48%), Memory - 10 MB (100.00%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -18,14 +18,41 @@ public:
     {
         if(nums.size() == 0 || nums.size() == 1)
             return;
-        vector<int> Copy = nums;
-        int shiftAmt = k;
-        // i --> (i + k) %n
-        for(int i = 0; i < nums.size(); i++)
+        // vector<int> Copy = nums;
+        // int shiftAmt = k;
+        // // i --> (i + k) %n
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     int newIndex = (i + shiftAmt)%nums.size();
+        //     nums[newIndex] = Copy[i];
+        // }
+        
+        
+        int startIndex, nextIndex, numShifted;
+        int cur = 0;
+        startIndex = numShifted = 0;
+        int temp = nums[0];
+        
+        while(numShifted != nums.size())
         {
-            int newIndex = (i + shiftAmt)%nums.size();
-            nums[newIndex] = Copy[i];
+            cur = startIndex;
+            temp = nums[startIndex];
+            
+            do
+            {
+                nextIndex = (cur + k)%nums.size();
+                
+                swap(temp, nums[nextIndex]);
+                
+                cur = nextIndex;
+                numShifted++;
+                
+
+            }while(cur != startIndex);
+            
+            startIndex++;
         }
+        
         
     }
 };
