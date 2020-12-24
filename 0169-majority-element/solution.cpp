90031fbc967c7ec364f782c3ba4d16f34e058c90
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2020-12-24 17:43:32 +0000

    [LeetCode Sync] Runtime - 32 ms (6.21%), Memory - 19.9 MB (99.95%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
+
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> num_count;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            num_count[nums[i]] = 0;
-        }
-        for(int i = 0; i < nums.size(); i++)
-        {
-            num_count[nums[i]] ++;
-        }
         
-        auto itr = num_count.begin();
-        while(itr != num_count.end())
+        int count = 0;
+        int result = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(itr->second >= (nums.size()/2 + 1))
-            {
-                return itr->first;
-            }
-            itr++;
+            if(count == 0)
+                result = nums[i];
+            
+            count += (nums[i] == result)? 1: -1;
         }
-        return 0;
+        return result;
+        
     }
 };
