198c666b77b30b6d1f0e409d4b2c6e1c1f1455b0
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-02-21 03:55:56 +0000

    [LeetCode Sync] Runtime - 4 ms (0.22%), Memory - 5.9 MB (100.00%)

--- 1236-n-th-tribonacci-number/solution.cpp
+++ 1236-n-th-tribonacci-number/solution.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-
-        if(n == 0)
-            return 0;
-        else if(n == 1)
-            return 1;
-        else if(n == 2)
-            return 1;
-        else
+    int tribonacci(int n) 
+    {
+    
+        long long trib[3] = {0,1, 1};
+     
+        for(int i = 0; i < n; i++)
         {
-            int a, b, c;
-            a = 0;
-            b = c = 1;
-            
-            for(int i = 0; i < n-2; i++){
-                int temp_a, temp_b;
-                temp_a = a;
-                a = b;
-                temp_b = b;
-                b = c;            
-                c = temp_a + temp_b + c;            
-            }
-            
-            return c;
+            long long firstTerm = trib[0];
+            long long secondTerm = trib[1];
+            trib[0] = trib[1];
+            trib[1] = trib[2];
+            trib[2] = trib[2] + firstTerm + secondTerm;
         }
+        
+        return static_cast<int>(trib[0]);
         
     }
 };
