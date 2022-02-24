209d2807c6105fabf754a2cc7bfaf202185a8900
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-02-24 05:59:44 +0000

    [LeetCode Sync] Runtime - 42 ms (6.19%), Memory - 19.7 MB (99.95%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -1,19 +1,24 @@
 class Solution {
-
 public:
-    int majorityElement(vector<int>& nums) {
-        
-        int count = 0;
+    int majorityElement(vector<int>& nums) 
+    {
         int result = 0;
+        int countOnes = 0;
         
-        for(int i = 0; i < nums.size(); i++)
+        for(int bitShift = 31; bitShift >= 0; bitShift--)
         {
-            if(count == 0)
-                result = nums[i];
-            
-            count += (nums[i] == result)? 1: -1;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                if((nums[i]>>bitShift)&1 == 1)
+                    countOnes++;
+            }
+            if(countOnes > nums.size()/2)
+            {
+                result += (1)<<bitShift;
+            }
+            countOnes = 0;
         }
-        return result;
         
+        return result;
     }
 };
