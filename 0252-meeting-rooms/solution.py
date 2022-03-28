7f0ae5533559a65ae5c53aefaa71686a1b24ba6a
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-03-28 04:01:29 +0000

    [LeetCode Sync] Runtime - 63 ms (9.38%), Memory - 16.8 MB (5.99%)

---
+++ 0252-meeting-rooms/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        intervals.sort(key=lambda x:x[0])
+        
+        for i in xrange(1,len(intervals)):
+            if intervals[i-1][0] <= intervals[i][0] < intervals[i-1][1]:
+                return False
+
+        return True
