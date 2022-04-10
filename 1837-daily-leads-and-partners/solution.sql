3f8f2af06da095fabb3da9730a787d5d441bdb38
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-10 03:23:14 +0000

    [LeetCode Sync] Runtime - 445 ms (100.00%), Memory - 0.0B (100.00%)

---
+++ 1837-daily-leads-and-partners/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT date_id, make_name, count(distinct(lead_id)) as unique_leads, count(distinct(partner_id)) as unique_partners
+FROM DailySales
+GROUP BY date_id, make_name
+ORDER BY make_name DESC, date_id DESC
