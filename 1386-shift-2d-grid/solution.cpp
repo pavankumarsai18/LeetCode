1b4c312505974d9142df9451754e60778ec2bed6
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-11 02:22:47 +0000

    [LeetCode Sync] Runtime - 20 ms (8.95%), Memory - 13.9 MB (100.00%)

--- 1386-shift-2d-grid/solution.cpp
+++ 1386-shift-2d-grid/solution.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) 
     {
-        vector<vector<int>> Temp = grid;
         
-        for(int i = 0; i < k; i++)
+        const int numRows = grid.size();
+        const int numCols = grid[0].size();
+        k = k%(numRows*numCols);
+        
+        int a = numRows*numCols;
+        int b = k;
+        const int N = a;
+        while(b)
         {
-            vector<vector<int>> temp;
-            for(int l = 0; l < grid.size(); l++)
-            {
-                vector<int> A(grid[0].size(),0);
-                temp.push_back(A);
-            }
-            
-            for(int row = 0; row < grid.size(); row++)
-            {
-                for(int col = 0; col < grid[0].size(); col++)
-                {
-                    if(col != grid[0].size() - 1)
-                         temp[row][col + 1] = Temp[row][col];
-                    else
-                    {
-                        if(row == grid.size() - 1)
-                            temp[0][0] = Temp[row][col];
-                        else
-                           temp[row+1][0] =  Temp[row][col];
-                    }
-                }
-            }
-            
-            Temp = temp;
+            int temp = b;
+            b = a%b;
+            a = temp;
         }
-        return Temp;
+        
+        int gcd = a;
+        
+        for(int i = 0; i < gcd; i++)
+        {
+            int index = i%N;
+            int copy = grid[index/numCols][index%numCols];
+            
+            for(int j = 1; j < N/gcd +1; j++)
+            {
+                index = (i+j*k)%N;
+                std::swap(grid[index/numCols][index%numCols], copy);
+            }
+        }
+       
+        
+        return grid;
         
     }
 };
