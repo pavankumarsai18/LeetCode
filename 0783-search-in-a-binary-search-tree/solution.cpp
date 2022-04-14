289547b4a6ee44abac695d631801e119ecc72e95
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-14 21:04:28 +0000

    [LeetCode Sync] Runtime - 42 ms (0.15%), Memory - 34.7 MB (99.82%)

--- 0783-search-in-a-binary-search-tree/solution.cpp
+++ 0783-search-in-a-binary-search-tree/solution.cpp
@@ -4,32 +4,36 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        TreeNode* result = nullptr;
-        TreeNode* curr_node = root;
-        while(curr_node != nullptr)
+        if(root == nullptr)
         {
-            if(curr_node->val < val)
+            return nullptr;
+        }
+        TreeNode* curNode = root;
+        
+        while(curNode)
+        {
+            if(curNode->val == val)
             {
-                curr_node = curr_node->right;
+                return curNode;
             }
-            else if(curr_node->val > val)
+            else if(curNode->val > val)
             {
-                curr_node = curr_node->left;
+                curNode = curNode->left;
             }
-            else
+            else if(curNode->val < val)
             {
-                result = curr_node;
-                break;
+                curNode = curNode->right;
             }
         }
-        return result;
-                                        
         
+        return nullptr;
     }
 };
