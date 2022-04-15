aa03044e7ee7add1d7cc09f253cbf6d0ab21bb4a
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-15 23:41:16 +0000

    [LeetCode Sync] Runtime - 27 ms (0.40%), Memory - 24 MB (87.82%)

--- 0669-trim-a-binary-search-tree/solution.cpp
+++ 0669-trim-a-binary-search-tree/solution.cpp
@@ -4,136 +4,32 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    void postorder_trim(TreeNode* & n, int L, int R)
-    {
-        if(n == nullptr)
-            ;//do nothing
-        else 
-        {
-            if(n->left == nullptr && n->right == nullptr)
-            {
-                if(n->val < L || n->val > R)
-                {
-                    delete n;
-                    n = nullptr;
-                }
-            }
-            else if(n->left != nullptr && n->right == nullptr)
-            {
-                postorder_trim(n->left, L, R);
-                if(n->val < L || n->val > R)
-                {
-                    
-                    if(n->left == nullptr){
-                        delete n;
-                        n = nullptr;
-                    }
-                    else
-                    {
-                        auto temp_val = n->left->val;
-                        auto temp_left = n->left->left;
-                        auto temp_right = n->left->right;
-                        
-                        delete n->left;
-                        n->val = temp_val;
-                        n->left = temp_left;
-                        n->right = temp_right;
-                        
-                        
-                    }
-                        
-                }
-            }
-            else if(n->left == nullptr && n->right != nullptr)
-            {
-                postorder_trim(n->right, L, R);
-                if(n->val < L || n->val > R)
-                {
-                    
-                    
-                    if(n->right == nullptr){
-                        delete n;
-                        n = nullptr;
-                    }
-                    else 
-                    {
-                        auto temp_val = n->right->val;
-                        auto temp_left = n->right->left;
-                        auto temp_right = n->right->right;
-                        
-                        delete n->right;
-                        n->val = temp_val;
-                        n->left = temp_left;
-                        n->right = temp_right;
-                        
-                        
-                    }
-                        
-                }
-                
-            }
-            else
-            {
-                postorder_trim(n->left, L, R);
-                postorder_trim(n->right, L, R);
-                
-                if(n->val < L || n->val > R)
-                {
-                    if(n->right == nullptr && n->left == nullptr)
-                    {
-                        delete n;
-                        n = nullptr;
-                    }
-                    else if(n->right == nullptr && n->left != nullptr)
-                    {
-                        auto temp_val = n->left->val;
-                        auto temp_l = n->left->left;
-                        auto temp_r = n->left->right;
-                        
-                        delete n->left;
-                        n->val = temp_val;
-                        n->left = temp_l;
-                        n->right = temp_r;
-                    }
-                    else if(n->right != nullptr && n->left == nullptr)
-                    {
-                        auto temp_val = n->right->val;
-                        auto temp_l = n->right->left;
-                        auto temp_r = n->right->right;
-                        
-                        delete n->right;
-                        n->val = temp_val;
-                        n->left = temp_l;
-                        n->right = temp_r;
-                    }
-                    else
-                    {
-                        auto temp_val = n->left->val;
-                        auto temp_l = n->left->left;
-                        auto temp_r = n->left->right;
-                        
-                        delete n->left;
-                        n->val = temp_val;
-                        n->left = temp_l;
-                        n->right = temp_r;
-                    }
-                }
-            }
-        }
-    }
 public:
-    TreeNode* trimBST(TreeNode* root, int L, int R) 
+    TreeNode* trimBST(TreeNode* root, int low, int high) 
     {
+        
         if(root == nullptr)
             return nullptr;
-        // need to perform a preorder search
-        // and delete nodes that are not not in range(L, R)
-        postorder_trim(root, L, R);
+        
+        if(root->val < low)
+        {
+            return trimBST(root->right, low, high);
+        }
+        else if(root->val > high)
+        {
+            return trimBST(root->left, low, high);
+        }
+        else
+        {
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+        }
         return root;
         
     }
