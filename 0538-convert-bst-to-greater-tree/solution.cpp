1e934893ee19df610f04e62eb41057cbff692a39
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-16 19:24:14 +0000

    [LeetCode Sync] Runtime - 34 ms (7.14%), Memory - 34.1 MB (100.00%)

--- 0538-convert-bst-to-greater-tree/solution.cpp
+++ 0538-convert-bst-to-greater-tree/solution.cpp
@@ -4,10 +4,13 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+    class Solution {
 private:
     void inorder(TreeNode* &root, vector<TreeNode*>& M, int& sum)
     {
