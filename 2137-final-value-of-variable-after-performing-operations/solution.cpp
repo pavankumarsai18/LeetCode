1c2ccdb3668ae8fe09555fdb2de57d50ea9c6ff2
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-04-24 16:39:18 +0000

    [LeetCode Sync] Runtime - 10 ms (0.96%), Memory - 14 MB (100.00%)

---
+++ 2137-final-value-of-variable-after-performing-operations/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) 
+    {
+        int result = 0;
+        
+        for(int i = 0; i < operations.size(); i++)
+        {
+            
+            if(operations[i][1] == '+')
+            {
+                result++;
+            }
+            else
+            {
+                result--;
+            }
+        }
+        
+        return result;
+    }
+};
