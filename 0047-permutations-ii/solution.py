97b2e40d6afd4597595937439ad0d5701aa38af5
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-05-12 03:41:57 +0000

    [LeetCode Sync] Runtime - 48 ms (22.43%), Memory - 13.5 MB (5.57%)

--- 0047-permutations-ii/solution.py
+++ 0047-permutations-ii/solution.py
@@ -1,50 +1,38 @@
 class Solution(object):
-    def permute(self, nums):
-        if len(nums) == 0:
-            return [[]]
-        
-        if len(nums) == 1:
-            return [nums]
-        if len(nums) == 2:
-            a, b = nums[0], nums[1]
-            return [[a,b],[b,a]]
-        
-        result = set()
-        num = nums[0]
-        items = self.permute(nums[1:])
-        for j in xrange(len(items)):
-            for i in xrange(len(nums)):
-                temp = list(copy.deepcopy(items[j]))
-                temp.insert(i, num)
-                result.add(tuple(temp))
-        return list(result)
-        
+    def stringify(self, nums):
+        result = ""
+        for num in nums:
+            result += str(num) + ","
+        return result
+    
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
-        numCount = dict()
-        for num in nums:
-            if num in numCount:
-                numCount[num] += 1
-            else:
-                numCount[num] = 1
-        
-        dupNums = []
-        for num in numCount:
-            for i in range(numCount[num]):
-                dupNums.append(str(num) + ":"+str(i))
-                
-        result = set()
-        dupPermutations = self.permute(dupNums)
-        for item in dupPermutations:
-            permutation = []
-            for s in item:
-                permutation.append(int(s[:s.find(':')]))
-            result.add(tuple(permutation))
-        
-        result = [list(item) for item in result]
+        result = []
+        def backtrack(comb, counter):
+            if len(comb) == len(nums):
+                result.append(list(comb))
+                return
+            
+            for num in counter:
+                if counter[num] > 0:
+                    comb.append(num)
+                    counter[num] -= 1
+                    
+                    backtrack(comb, counter)
+                    
+                    comb.pop()
+                    counter[num] += 1
+                    
+        backtrack([], Counter(nums))
         
         return result
+                    
+                    
+                                
+
+        
+        
+        
