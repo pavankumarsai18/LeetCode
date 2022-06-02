8f3ef15d16d40b98b8562a8cb788e19357d63481
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-02 01:08:19 +0000

    [LeetCode Sync] Runtime - 13 ms (0.59%), Memory - 10.9 MB (100.00%)

--- 0898-transpose-matrix/solution.cpp
+++ 0898-transpose-matrix/solution.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& A)
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) 
     {
-        int num_rows = A.size();
-        int num_cols = A[0].size();
 
-        vector<vector<int>> result(num_cols);
         
-        // cout << num_rows<<" "<< num_cols<<endl;
-        // cout << result.size() << " " << result[0].size()<<endl;
-        for(int row = 0; row < num_rows; row++)
+        vector<vector<int>> result;
+        for(int j = 0; j < matrix[0].size(); j++)
         {
-            //Swap the elements in the diag row to the diag col
-            for(int col = 0; col < num_cols; col++)
+            vector<int> row;
+            for(int i = 0; i < matrix.size(); i++)
             {
-                result[col].push_back(A[row][col]);
+                 row.push_back(matrix[i][j]);
             }
+            result.push_back(row);
         }
-        
         return result;
     }
 };
