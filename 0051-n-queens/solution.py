53dfaf76f5fb202e4a4069cac44d6d3aad606236
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-04 02:09:12 +0000

    [LeetCode Sync] Runtime - 79 ms (7.89%), Memory - 14.5 MB (100.00%)

--- 0051-n-queens/solution.py
+++ 0051-n-queens/solution.py
@@ -1,61 +1,74 @@
-class Solution(object):
-    def initBoard(self):
-        self.diags           = set()
-        self.antiDiags       = set()
-        self.cols = set()
-        self.numQueens       = 0
-        self.board = [['.' for _ in range(self.n)] for __ in range(self.n)]
+class Solution:
     
-    def isValid(self, r, c):
-        return  (c not in self.cols) and (r+c not in self.diags) and (r - c not in self.antiDiags) 
-    
-    def getBoard(self):
-        ans = []
-        for row in self.board:
-            ans.append("".join(row))
-        return tuple(ans)
-    
-    def printBoard(self):
-        for row in self.board:
-            print(row)
-        print("COLS", self.cols)
-        print("DIAGS", self.diags)
-        print("ANTIDIAGS", self.antiDiags)
+    def solveNQueens(self, n: int) -> List[List[str]]:
         
-    def backtrack(self, r):
-        # self.printBoard()
+        if n == 1:
+            return [["Q"]]
         
-        if r == self.n:
-            self.result.append(self.getBoard())
-            return
-        
-        for c in range(self.n):            
-            if self.isValid(r, c):
-                self.cols.add(c)
-                self.diags.add(r+c)
-                self.antiDiags.add(r-c)
-                
-                self.board[r][c] = 'Q'
-                
-                self.backtrack(r+1)
-                
-                self.cols.remove(c)
-                self.diags.remove(r+c)
-                self.antiDiags.remove(r-c)
-                self.board[r][c] = "."
-                
+        self.n         = n
+        self.result    = []
+        self.numQueens = 0
+       
+        def canAddSafely(r, c):
+            diag = r + c
+            antiDiag = r - c
             
-    
-    def solveNQueens(self, n):
-        """
-        :type n: int
-        :rtype: List[List[str]]
-        """
-        self.n = n
-        self.result = []
+            safe = (c not in self.cols)
+            safe = safe and (diag not in self.diags)
+            safe = safe and (antiDiag not in self.antiDiags)
+            
+            return safe
+        
+        def addQueen(r, c):
+            self.board[r][c] = 'Q'
+            self.numQueens += 1
 
-        self.initBoard()
-        self.backtrack(0)
+            self.antiDiags.add(r-c)
+            self.diags.add(r+c)
+            self.cols.add(c)
+        
+        def deleteQueen(r, c):
+            self.board[r][c] = '.'
+            self.numQueens -= 1
+
+            self.antiDiags.remove(r-c)
+            self.diags.remove(r+c)
+            self.cols.remove(c)
+        
+        def printBoard():
+            for row in self.board:
+                print(row)
+            print("\n\n")
+        
+        def solve():
+            for c in range(self.n):
+                if canAddSafely(self.row, c):
+                    addQueen(self.row, c)
+                    
+                    if self.row == self.n - 1 and self.numQueens == self.n:
+                        self.result.append(["".join(row) for row in copy.deepcopy(self.board)])
+                    
+                    elif self.row != self.n-1:
+                        self.row += 1
+                        solve()
+                        self.row -= 1
+                    
+                    deleteQueen(self.row, c)
+                    
+        
+        for i in range(self.n):
+            self.cols, self.diags, self.antiDiags = set(), set(), set()        
+            self.board = [["."]*self.n for _ in range(self.n)]
+            self.numQueens = 0
+            self.row = 0
+            
+            addQueen(0, i)
+            
+            self.row = 1
+            
+            solve()
+            
             
         return self.result
+        
         
