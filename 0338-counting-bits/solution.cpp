9b652a58e23a2a67c3e18811476a88d9e6e67981
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-06 22:44:40 +0000

    [LeetCode Sync] Runtime - 9 ms (5.60%), Memory - 8.6 MB (100.00%)

--- 0338-counting-bits/solution.cpp
+++ 0338-counting-bits/solution.cpp
@@ -1,50 +1,23 @@
 class Solution {
-private:
-    int increment(vector<int> & arr)
-    {
-        int carry = 1;
-        for(int i =  0; i < arr.size(); i++)
-        {
-            if(arr[i] == 0)
-            {
-                if(carry == 1)
-                {
-                    arr[i] = 1;
-                    break;
-                }
-            }
-            else if(arr[i] == 1)
-            {
-                if(carry == 1)
-                {
-                    arr[i] = 0;
-                    carry = 1;
-                }
-            }
-        }
-        
-        int num_ones = 0;
-        for(int i = 0; i < arr.size(); i++)
-        {
-            if(arr[i] == 1)
-                num_ones++;
-        }
-        return num_ones;
-    }
 public:
-    vector<int> countBits(int num) 
+    vector<int> countBits(int n) 
     {
-        vector<int> arr;
-        for(int i = 0; i < 32; i++)
+        if(n == 0)
         {
-            arr.push_back(0);
+            return {0};
         }
         
-        vector<int> result;
-        result.push_back(0);
-        for(int i = 1; i <= num; i++)
+        vector<int> result = {0,1};
+        
+        while(result.size() < (n+1))
         {
-            result.push_back(increment(arr));
+            int curSize = result.size();
+            int i       = 0;
+            while(i < curSize && result.size() < (n+1))
+            {
+                result.push_back(result[i] + 1);
+                i++;
+            }
         }
         return result;
     }
