9ec9cfe494ceb98025a1fb07ef13dc9d4fe08bb0
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-06 01:06:11 +0000

    [LeetCode Sync] Runtime - 20 ms (5.46%), Memory - 13.7 MB (100.00%)

--- 0347-top-k-frequent-elements/solution.cpp
+++ 0347-top-k-frequent-elements/solution.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
-        unordered_map<int, int> numCount;
-        for(auto & n: nums)
-        {
-            numCount[n]++;
+        unordered_map<int, int> num_count;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {            
+            num_count[nums[i]]++;
         }
-        priority_queue<pair<int, int>> Q;
-        for(auto item:numCount)
+        
+        auto cmp = [&num_count](int & a, int & b){return num_count[a] < num_count[b];};
+        
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+        for(auto [num, count]: num_count)
         {
-            Q.push({item.second, item.first});
+            pq.push(num);
         }
+        
         vector<int> result;
-        for(int i =0;i<k;i++)
+        while(result.size() < k && pq.size() > 0)
         {
-            result.push_back(Q.top().second);Q.pop();
+            result.push_back(pq.top());
+            pq.pop();
         }
+        
         return result;
+        
     }
 };
