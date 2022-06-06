0e967339decc55f70774a94f31eb2fc37d3c9fd7
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-06 21:13:26 +0000

    [LeetCode Sync] Runtime - 13 ms (2.70%), Memory - 15.7 MB (100.00%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -2,54 +2,24 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) 
     {
+        int lo, hi;
+        lo = 0; hi = numbers.size()-1;
         
-        unordered_map<int, int> num_count;
-        for(int i = 0; i < numbers.size(); i++)
+        while(lo < hi)
         {
-            auto itr = num_count.find(numbers[i]);
-            if(itr == num_count.end())
-                num_count[numbers[i]] = i;
-        }
-        
-        
-        for(int i = 0; i < numbers.size(); i++)
-        {
-            if(target - numbers[i] == numbers[i])
+            if(numbers[lo] + numbers[hi] > target)
             {
-                if(i > 0 && numbers[i-1] == numbers[i])
-                {
-                    vector<int> res;
-                    res.push_back(i);
-                    res.push_back(i + 1);
-                    return res;
-                }
-                else if( i < numbers.size() - 1 && numbers[i] == numbers[i + 1])
-                {
-                    vector<int> res;
-                    res.push_back(i + 1);
-                    res.push_back(i + 2);
-                    return res;
-                }
+                hi--;
+            }
+            else if(numbers[lo] + numbers[hi] < target)
+            {
+                lo++;
             }
             else
             {
-                auto itr = num_count.find(target - numbers[i]);
-
-                if(itr != num_count.end())
-                {
-                    if(itr->second > i)
-                    {
-                        vector<int> result;
-                        result.push_back(i + 1);
-                        result.push_back(itr->second + 1);
-                        return result;
-                    }
-                }
+                return {lo+1, hi+1};
             }
         }
         return vector<int>();
-        
-        
-        
     }
 };
