bdd699d89296b362d251d9ee06a8e45b8641d594
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-06 01:12:07 +0000

    [LeetCode Sync] Runtime - 49 ms (6.26%), Memory - 27.8 MB (100.00%)

--- 0238-product-of-array-except-self/solution.cpp
+++ 0238-product-of-array-except-self/solution.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        
-        vector<int> result(nums.size(), 1);
-
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        vector<int> prefix, postfix;
+        long long curProduct = 1;
         for(int i = 0; i < nums.size(); i++)
         {
-            if(i == 0)
-            {
-                result[i] = 1;
-            }
-            else{
-                result[i] = result[i-1]*nums[i-1];
-            }
+            prefix.push_back(curProduct);
+            curProduct *= nums[i];
         }
         
-        int R = 1;
-        for(int i = nums.size() - 1; i >= 0; i--)
+        
+        curProduct = 1;
+        for(int i = nums.size()-1; i >= 0; i--)
         {
-            
-            if(i == nums.size() - 1)
-            {
-                R = 1;
-            }
-            else{
-                R *= nums[i+1];
-            }
-            result[i] *= R;
+            postfix.push_back(curProduct);
+            curProduct *= nums[i];
+
         }
         
+        reverse(postfix.begin(), postfix.end());
         
-    
+        vector<int> result;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            result.push_back(postfix[i]*prefix[i]);
+        }
+        
         return result;
     }
-    
 };
