48e67026b57013cb28b0b4ececeb8240e27462da
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-16 00:34:19 +0000

    [LeetCode Sync] Runtime - 110 ms (5.70%), Memory - 15.7 MB (100.00%)

---
+++ 0116-populating-next-right-pointers-in-each-node/solution.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        leftMost = root
+        
+        while leftMost.left:
+            head = leftMost
+            
+            while head:
+                head.left.next = head.right
+                
+                if head.next:
+                    head.right.next = head.next.left
+                    
+                head = head.next
+            leftMost = leftMost.left
+        
+        return root
