49defb9478f2eec966fb7c66158f0e6d1b218031
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-16 05:41:35 +0000

    [LeetCode Sync] Runtime - 134 ms (6.71%), Memory - 18.8 MB (100.00%)

--- 0285-inorder-successor-in-bst/solution.py
+++ 0285-inorder-successor-in-bst/solution.py
@@ -1,39 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def inorder(self, node, p):
-        if not node:
-            return
+class Solution:
+    
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
         
-        # Recurse on the left side
-        self.inorder(node.left, p)
+        successor = None
         
-        # Check if previous is the inorder predecessor of node
-        if self.previous == p and not self.inorder_successor_node:
-            self.inorder_successor_node = node
-            return
-        
-        # Keeping previous up-to-date for further recursions
-        self.previous = node
-        
-        # Recurse on the right side
-        self.inorder(node.right, p)
-    def inorderSuccessor(self, root, p):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :rtype: TreeNode
-        """
-        self.previous,self.inorder_successor_node = None,None
-        if p.right:
-            cur = p.right
-            while cur.left:
-                cur = cur.left
-            return cur
-        self.inorder(root, p)
-        return self.inorder_successor_node
+        while root:
+            
+            if p.val >= root.val:
+                root = root.right
+            else:
+                successor = root
+                root = root.left
+                
+        return successor
