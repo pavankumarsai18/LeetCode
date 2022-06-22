6b41768c1bcbdd0d52fe33449636c6408f412dfe
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-22 00:32:49 +0000

    [LeetCode Sync] Runtime - 7 ms (95.72%), Memory - 9.9 MB (100.00%)

--- 0215-kth-largest-element-in-an-array/solution.cpp
+++ 0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    
     int findKthLargest(vector<int>& nums, int k) 
     {
         make_heap(nums.begin(), nums.end());
-        int i = 0;
-        
-        while(i < k - 1)
+        for(int i = 0; i < k-1; i++)
         {
             pop_heap(nums.begin(), nums.end());
             nums.pop_back();
-            i++;
         }
         
-        return nums.front();
+        return nums[0];
+        
     }
 };
