87681c750d92a4d4ab3f9a04f4095f03996a28b0
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-24 22:20:57 +0000

    [LeetCode Sync] Runtime - 7 ms (47.63%), Memory - 10.8 MB (99.99%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,61 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numCount;
+        vector<int> ans;
         
-        unordered_map<int, int> num_count;
-        unordered_map<int, vector<int>> num_indicies;
         for(int i = 0; i < nums.size(); i++)
         {
-            auto itr = num_count.find(nums[i]);
-            
-            auto itr2 = num_indicies.find(nums[i]);
-            if(itr == num_count.end())
+            if(numCount.find(target-nums[i]) != numCount.end())
             {
-                num_count[nums[i]] = 1;
-                
+                ans = {i, numCount[target-nums[i]]};
+                return ans;
             }
-            else if(itr != num_count.end()){
-                num_count[nums[i]]++;
-                
-            }
-            if(itr2 == num_indicies.end())
-            {
-                num_indicies[nums[i]] = vector<int>();
-                (num_indicies[nums[i]]).push_back(i);
-            }
-            if(itr2 != num_indicies.end())
-            {
-                (num_indicies[nums[i]]).push_back(i);
-            }
+            numCount[nums[i]] = i;
+                   
         }
-        
-        vector<int> result;
-        
-        for(auto itr = num_count.begin(); itr != num_count.end(); itr++)
-        {
-            int secondNumber = target - itr->first;
-            
-            auto secondItr = num_count.find(secondNumber);
-            
-            if(secondItr != num_count.end())
-            {
-                if(secondNumber == itr->first && itr->second >= 2)
-                {
-                    vector<int> indicies = num_indicies[secondNumber];
-                    result.push_back(indicies[0]);
-                    result.push_back(indicies[1]);
-                    return result;
-                }
-                else if(secondNumber != itr->first)
-                {
-                    result.push_back((num_indicies[secondNumber])[0]);
-                    result.push_back((num_indicies[itr->first])[0]);
-                    return result;
-                }
-            }
-        }
-        
-        return result;
+        return ans;
         
     }
 };
