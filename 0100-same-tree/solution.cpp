2264a71e5aeafb632af5c735aed3b7b567861add
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-06-29 05:26:22 +0000

    [LeetCode Sync] Runtime - 11 ms (0.07%), Memory - 10.2 MB (99.97%)

--- 0100-same-tree/solution.cpp
+++ 0100-same-tree/solution.cpp
@@ -4,35 +4,53 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    bool preorder(TreeNode* &p, TreeNode* &q)
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
     {
-        if(p == nullptr && q != nullptr)
-            return false;
-        if(p != nullptr && q == nullptr)
-            return false;
         if(p == nullptr && q == nullptr)
             return true;
-        else if(p != nullptr && q != nullptr)
+        if(p == nullptr ^ q == nullptr)
         {
-            if(p->val == q->val)
+            return false;
+        }
+
+        stack<TreeNode*> PStack;
+        stack<TreeNode*> QStack;
+
+        PStack.push(p);
+        QStack.push(q);
+
+        while(PStack.size() && QStack.size())
+        {
+            TreeNode* curP = PStack.top(); PStack.pop();
+            TreeNode* curQ = QStack.top(); QStack.pop();
+
+            if((!curP && !curQ) || (curP && curQ && curP->val == curQ->val))
             {
-                return preorder(p->left, q->left) &&  preorder(p->right, q->right);
+                if(curP && curQ)
+                {
+                    PStack.push(curP->right);
+                    PStack.push(curP->left);
+                    QStack.push(curQ->right);
+                    QStack.push(curQ->left);
+                }
             }
             else
             {
                 return false;
             }
         }
+        if(PStack.empty() && QStack.empty())
+            return true;
         return false;
-    }
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) 
-    {
+
+
         
-        return preorder(p, q);
     }
 };
