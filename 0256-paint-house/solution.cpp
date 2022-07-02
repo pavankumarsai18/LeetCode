befd2d9a5ec1d470d35550848afdea47300caab9
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-02 07:02:26 +0000

    [LeetCode Sync] Runtime - 7 ms (7.17%), Memory - 9.6 MB (100.00%)

--- 0256-paint-house/solution.cpp
+++ 0256-paint-house/solution.cpp
@@ -2,20 +2,32 @@ class Solution {
 public:
     int minCost(vector<vector<int>>& costs) 
     {
+        const int n = costs.size();
+        const int k = 3;
         
-        int curCost[] = {0,0,0};
-        int prevCost[] = {0,0,0};
+        int dp[3];
+        int newDP[3];
         
-        for(int i = 0; i < costs.size(); i++)
+        dp[0] = costs[0][0];
+        dp[1] = costs[0][1];
+        dp[2] = costs[0][2];
+        
+        for(int i = 1; i < n; i++)
         {
-            for(int j = 0; j < 3; j++)
+            newDP[0] = newDP[1] = newDP[2] = 0;
+            
+            for(int j = 0; j < k; j++)
             {
-                curCost[j] = costs[i][j]+min(prevCost[(j+1)%3],prevCost[(j+2)%3]);   
+                newDP[j] = min(dp[(j+1)%k], dp[(j+2)%k]) + costs[i][j];
             }
-            for(int j = 0; j < 3; j++)
-                prevCost[j] = curCost[j];
+            
+            for(int j = 0; j < k; j++)
+            {
+                dp[j] = newDP[j];
+            }
         }
         
-        return min(curCost[0], min(curCost[1], curCost[2]));
+        
+        return min(min(dp[0], dp[1]), dp[2]);
     }
 };
