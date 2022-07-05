806727a8aa350ec503e75b9d7efd54d789e05fd9
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-05 01:41:21 +0000

    [LeetCode Sync] Runtime - 2588 ms (5.01%), Memory - 29.6 MB (11.79%)

--- 0128-longest-consecutive-sequence/solution.py
+++ 0128-longest-consecutive-sequence/solution.py
@@ -4,26 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return 1
-
-        setNums        = set(nums)
         
+        setNums = set(nums)
         longestSequence = 0
-        for num in setNums:     
+        
+        for num in nums:
             if num-1 not in setNums:
                 cur = num
-                lengthSequence  = 1
-                while cur+1 in setNums:
-                    cur = cur + 1
-                    lengthSequence += 1                
-                longestSequence = max(lengthSequence, longestSequence)
-        
-        return longestSequence
-                
-                    
-                
-        
+                length = 1
+                while cur + 1 in setNums:
+                    length += 1
+                    cur += 1
 
+                longestSequence = max(longestSequence, length)
+        return longestSequence
