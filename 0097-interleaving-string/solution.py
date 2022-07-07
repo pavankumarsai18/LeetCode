3882be5bd2acf0f61fd0f0f102de7a59f73d729e
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-07 07:06:12 +0000

    [LeetCode Sync] Runtime - 81 ms (5.97%), Memory - 14 MB (100.00%)

--- 0097-interleaving-string/solution.py
+++ 0097-interleaving-string/solution.py
@@ -1,18 +1,22 @@
-class Solution(object):
-    def isInterleave(self, A, B, C):
+class Solution:
+    def isInterleave(self, A: str, B: str, C: str) -> bool:
         if len(A) + len(B) != len(C):
             return False
-
-        DP = [None for _ in range(len(B)+1)] 
-
+        
+        DP = [True]*(len(B)+1)
+        
         for i in range(len(A)+1):
             for j in range(len(B)+1):
-                if i==0 and j==0:
+                if i == 0 and j == 0:
                     DP[j] = True
                 elif i == 0:
-                    DP[j] = (DP[j-1] and (C[i+j-1] == B[j-1]))
+                    DP[j] = DP[j - 1] and (C[i+j-1] == B[j-1])
                 elif j == 0:
-                    DP[j] = (DP[j] and (C[i+j-1] == A[i-1]))
+                    DP[j] = DP[j] and (C[i+j-1] == A[i-1])
                 else:
-                    DP[j] = (DP[j] and (C[i+j-1] == A[i-1])) or (DP[j-1] and (C[i+j-1] == B[j-1]))
+                    DP[j] = (DP[j - 1] and C[i+j-1] == B[j-1]) or (DP[j] and C[i+j-1] == A[i-1]) 
+                    
         return DP[-1]
+                
+        
+        
