7a84f5013fc21e39d0030c6e58731aab643eeac3
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-28 03:12:06 +0000

    [LeetCode Sync] Runtime - 4 ms (40.86%), Memory - 7.4 MB (100.00%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.size() != t.size()) return false;
+        
+        const int numChars = 26;
+        int charCount[numChars] = {0};
+        for(int i = 0; i < s.size(); i++)
+        {
+            charCount[s[i] - 'a']++;
+        }
+        
+        
+        for(int i = 0; i < t.size(); i++)
+        {
+            charCount[t[i]-'a']--;
+        }
+        
+        
+        for(int i = 0; i < numChars; i++)
+        {
+            if(charCount[i] != 0) return false;
+        }
+        
+        return true;
+        
+    }
+};
