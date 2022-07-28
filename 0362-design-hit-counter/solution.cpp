12c81a3d8429f47a94e638084bca27aa668f579c
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-28 21:22:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.4 MB (100.00%)

---
+++ 0362-design-hit-counter/solution.cpp
@@ -0,0 +1,73 @@
+class HitCounter {
+private:
+    deque<pair<int, int>> queue;
+    int curTime;
+    int numHits;
+    const int threshold;
+public:
+    HitCounter(): threshold(300)
+    {
+        curTime = 0;
+        numHits = 0;
+    }
+    
+    void hit(int timestamp) 
+    {
+        if(queue.empty())
+        {
+            queue.push_back({timestamp, 1});
+            numHits++;
+        }
+        else
+        {
+            if(queue.back().first == timestamp)
+            {
+                pair<int, int> item = queue.back();
+                queue.pop_back();
+                queue.push_back({item.first, item.second + 1});
+                numHits++;
+                
+            }
+            else
+            {
+                queue.push_back({timestamp, 1});
+                numHits++;
+            }
+        }
+        
+        // print(queue);
+
+        
+        return;
+    }
+    void print(deque<pair<int, int>> Q)
+    {
+        while(!Q.empty())
+        {
+            cout<<"("<<Q.front().first<<","<<Q.front().second<<") ";
+            Q.pop_front();
+        }
+        cout<<endl;
+    }
+    
+    int getHits(int timestamp)
+    {
+        // cout<<"timestamp "<<timestamp<<endl;
+        // cout<<"(timestamp - front "<<timestamp - queue.front().first<<") "<<threshold<<endl;
+        while(!queue.empty() && timestamp - queue.front().first >= threshold)
+        {
+            numHits -= queue.front().second;
+            queue.pop_front();
+        }
+        // print(queue);
+
+        return numHits;
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
