e8ddb9f008cf5684bce6e127cfde23be28c9cabf
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-07-29 02:47:07 +0000

    [LeetCode Sync] Runtime - 9 ms (4.92%), Memory - 8.3 MB (100.00%)

--- 0926-find-and-replace-pattern/solution.cpp
+++ 0926-find-and-replace-pattern/solution.cpp
@@ -1,59 +1,36 @@
 class Solution {
 private:
-    bool Bijection(string& A, string& B)
+    bool match(string & word, string & pattern)
     {
-        if(A.size() != B.size())
-            return false;
-        
-        bool result = false;
-        
-        unordered_map<char, char> forward;
-        unordered_map<char, char> backward;
-        
-        // A[i] -> B[i] // forward
-        // A[i] <- B[i] // backward
-        
-        for(int i = 0; i < A.size(); i++)
+        unordered_map<char, char> word_pattern;
+        for(int i = 0; i < word.size(); i++)
         {
-            auto itr1 = forward.find(A[i]);
-            auto itr2 = backward.find(B[i]);
-            
-            if(itr1 == forward.end())
+            if(word_pattern.find(word[i]) == word_pattern.end())
             {
-                forward[A[i]] = B[i];
+                word_pattern[word[i]] = pattern[i];
             }
-            else
+            else if(word_pattern[word[i]] != pattern[i]) 
             {
-                if(forward[A[i]] != B[i])
-                    return false;
-            }
-            
-            if(itr2 == backward.end())
-            {
-                backward[B[i]] = A[i];
-            }
-            else
-            {
-                if(backward[B[i]] != A[i])
-                    return false;
+                return false;
             }
         }
-    
-        
         return true;
+    }
     
+    bool wordMatchesPattern(string & word, string & pattern)
+    {
+        if(word.size() != pattern.size()) return false;
+        return match(word, pattern) && match(pattern, word);
     }
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) 
     {
-        vector<string> result;
-        
-        for(auto word: words)
+        vector<string> ans;
+        for(int i = 0; i < words.size(); i++)
         {
-            if(Bijection(word, pattern))
-                result.push_back(word);
+            if(wordMatchesPattern(words[i], pattern)) ans.push_back(words[i]);
         }
         
-        return result;
+        return ans;
     }
 };
