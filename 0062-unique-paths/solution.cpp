5ca3796876aa77a0a99d391227b9809aa5ac51a2
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-08-01 06:05:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,47 +1,25 @@
 class Solution {
-private:
-    int uniquePathsCalculate(int m, int n, unordered_map<string, int> & memo)
-    {
-        string key = "";
-        key += std::to_string(m) + ","+std::to_string(n);
-        // for(auto & itr : memo)
-        // {
-        //     cout<<"("<<itr.first<<") -> "<<itr.second<<endl;
-        // }
-        if(memo.find(key) != memo.end())
-        {
-            return memo[key];
-        }
-        if(m == 0 || n == 0)
-        {
-            memo[key] = 0;
-            return 0;
-        }
-        
-        if(m == 1)
-        {
-            memo[key] = 1;
-            return 1;
-        }
-        else if(n == 1)
-        {
-            memo[key] = 1;
-            return 1;
-        }
-        
-        int result = uniquePathsCalculate(m-1, n, memo) + uniquePathsCalculate(m, n-1,memo);
-        // cout<<"result "<<result<<endl;
-        memo[key] = result;
-        
-        return result;
-        
-    }
 public:
     int uniquePaths(int m, int n) 
     {
-        unordered_map<string, int> memo;
-        return uniquePathsCalculate(m, n, memo);
+        const int numRows = m;
+        const int numCols = n;
         
+        
+        vector<int> prevRow(n, 1);        
+        for(int row = numRows - 2; row >= 0; row--)
+        {
+            vector<int> newRow(n, 1);
+            for(int col = numCols - 2; col >= 0; col--)
+            {
+                newRow[col]  = newRow[col+1];
+                newRow[col] += prevRow[col];
+            }
+            
+            prevRow = newRow;
+        }
+        
+        return prevRow[0];
         
     }
 };
