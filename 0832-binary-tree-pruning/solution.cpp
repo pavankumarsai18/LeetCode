ade5b2c5ad70daf011a425c469c69d89557f75be
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-09-06 16:06:49 +0000

    [LeetCode Sync] Runtime - 7 ms (0.00%), Memory - 8.7 MB (100.00%)

--- 0832-binary-tree-pruning/solution.cpp
+++ 0832-binary-tree-pruning/solution.cpp
@@ -4,60 +4,54 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    void postorder(TreeNode* &n)
+public:
+    void prune(TreeNode* node, int & numOnes)
     {
-        if(n == nullptr)
-            return;
-        else
+        if(node == nullptr) return;
+        int numOnesLeft, numOnesRight;
+        numOnesLeft = numOnesRight = 0;
+
+        if(node->left)
         {
-            if(n->left == nullptr && n->right != nullptr)
+            prune(node->left, numOnesLeft);
+            
+            if(numOnesLeft == 0)
             {
-                postorder(n->right);
-                if(n->right == nullptr && n->val == 0)
-                {
-                    delete n;
-                    n = nullptr;
-                }
-            }
-            else if(n->left == nullptr && n->right == nullptr)
-            {
-                if(n->val == 0)
-                {
-                    delete n;
-                    n = nullptr;
-                }
-            }
-            else if(n->left != nullptr && n->right == nullptr)
-            {
-                postorder(n->left);
-                if(n->left == nullptr && n->val == 0)
-                {
-                    delete n;
-                    n = nullptr;
-                }
-            }
-            else
-            {
-                postorder(n->left);
-                postorder(n->right);
-                if(n->left == nullptr && n->right == nullptr && n->val == 0)
-                {
-                    delete n;
-                    n = nullptr;
-                }
+                node->left = nullptr;
             }
         }
+        
+        if(node->right)
+        {
+            prune(node->right, numOnesRight);
+            
+            if(numOnesRight == 0)
+            {
+                node->right = 0;
+            }
+        }
+        
+        if(node->val == 1)
+            numOnes++;
+        
+        numOnes += numOnesLeft;
+        numOnes += numOnesRight;
+        
+        return;
     }
-public:
     TreeNode* pruneTree(TreeNode* root) 
     {
+        TreeNode * dummy = new TreeNode(1, root, nullptr);
+        int numOnes = 0;
+        prune(dummy, numOnes);
         
-        postorder(root);
-        return root;
+        return dummy->left;
+        
     }
 };
