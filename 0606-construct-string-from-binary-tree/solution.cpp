a4f2500d7d3b6fbe3dd5533d08cab0f5e4d586f3
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-09-07 16:32:31 +0000

    [LeetCode Sync] Runtime - 41 ms (7.50%), Memory - 53.8 MB (54.63%)

--- 0606-construct-string-from-binary-tree/solution.cpp
+++ 0606-construct-string-from-binary-tree/solution.cpp
@@ -4,101 +4,28 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    string tree2str(TreeNode* t) 
-    {    
-        if(t == nullptr)
-            return "";
-        //perform a preorder traversal
-        string result = "";
-        stringstream ss;
-        ss << t->val;
-        ss >> result;
-        if(t->left != nullptr)
-        {
-            result += "(";
-            preorder(t->left, result);
-            if(t->right != nullptr)
-            {
-                result += ")(";
-                preorder(t->right, result);
-                result += ")";
-            }
-            else
-                result += ")";
-        }
-        else
-        {
-            
-            if(t->right != nullptr)
-            {
-                result += "()(";
-                preorder(t->right, result);
-                result += ")";
-            }
-        }
-        return result;
-        
-    }
-    
-    void preorder(TreeNode*t, string & s)
+    string tree2str(TreeNode* root) 
     {
-        if(t == nullptr)
-            return;
-        else
+        string ans = "";
+        if(root == nullptr) return ans;
+        
+        ans += to_string(root->val);
+        if(root->left == nullptr && root->right == nullptr) return ans;
+        
+        ans += "(" + tree2str(root->left)+")";
+        if(root->right)
         {
-            stringstream ss;
-            if(t->left == nullptr && t->right == nullptr)
-            {
-                ss << t->val;
-                string temp = "";
-                ss >> temp;
-                s += temp;
-            }
-            else if(t->left == nullptr && t->right != nullptr)
-            {
-                ss << t->val;
-                string temp = "";
-                ss >> temp;
-
-                s += temp;
-                s += "()";
-                
-                s += "(";
-                preorder(t->right, s);
-                s += ")";
-            }
-            else if(t->right == nullptr && t->left != nullptr)
-            {
-                ss << t->val;
-                string temp = "";
-                ss >> temp;
-                
-
-                s += temp;
- 
-                s += "(";
-                preorder(t->left,s);
-                s += ")";
-                
-            }
-            else
-            {
-                ss << t->val;
-                string temp = "";
-                ss >> temp;
-                s += temp;
-                s += "(";
-                preorder(t->left, s);
-                s += ")(";
-                preorder(t->right, s);
-                s += ")";
-                
-            }
+            ans += "(" + tree2str(root->right) + ")";
         }
+        
+        return ans;
+        
     }
 };
