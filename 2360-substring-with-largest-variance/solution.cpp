b24383f4f5dc4fde2e15d6a0db09306af228f53b
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-09-09 16:03:00 +0000

    [LeetCode Sync] Runtime - 38 ms (94.72%), Memory - 6.9 MB (100.00%)

--- 2360-substring-with-largest-variance/solution.cpp
+++ 2360-substring-with-largest-variance/solution.cpp
@@ -2,51 +2,38 @@ class Solution {
 public:
     int largestVariance(string s) 
     {
-        // dp[c1][c2][0] stores the frequency of c1
-        // dp[c1][c2][1] stores the (freq(c1) - freq(c2))
-        // ans stores the answer
-        
-        
-        int dp[26][26][2] = {0};
+        int freq[26][26][2] = {0};
         
         for(int i = 0; i < 26; i++)
-        {
             for(int j = 0; j < 26; j++)
-            {
-                dp[i][j][1] = -10000; // initalize to minimum -10000 since size of string is at max 10,000
-            }
-        }
+                freq[i][j][1] = -10000;
         
         int ans = 0;
         
         for(int i = 0; i < s.size(); i++)
         {
-            int c1 = s[i] - 'a';
-            for(int c2 = 0; c2 < 26; c2++)
+            int a = s[i] - 'a';
+            for(int b = 0; b < 26; b++)
             {
-                if(c1 == c2) continue;
+                if(a == b) continue;
                 
+                // a & b
+                freq[a][b][0]++;
+                freq[a][b][1]++;
                 
-                // increment dp[c1][c2][0] since we have found c1
-                // increment dp[c1][c2][1] for same reason as above
-                dp[c1][c2][0]++;
-                dp[c1][c2][1]++;
+                // b & a
+                freq[b][a][1] = freq[b][a][0] - 1;
+                if(freq[b][a][0] > 0)
+                {
+                    freq[b][a][0]--;
+                }
                 
-                
-                // update c2 c1
-                dp[c2][c1][1] = dp[c2][c1][0] - 1;
-                
-        
-                if(dp[c2][c1][0] > 0)
-                    dp[c2][c1][0]--;
-                
-                
-                // get the maximum
-                ans = max(ans, dp[c2][c1][1]);
-                ans = max(ans, dp[c1][c2][1]);
+                ans = max(ans, freq[a][b][1]);
+                ans = max(ans, freq[b][a][1]);
             }
         }
         
         return ans;
+        
     }
 };
