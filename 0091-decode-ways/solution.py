480a2382fe37fadb73ce4489edbe83f3ba5dddad
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-10-01 13:53:20 +0000

    [LeetCode Sync] Runtime - 77 ms (3.81%), Memory - 14.4 MB (100.00%)

--- 0091-decode-ways/solution.py
+++ 0091-decode-ways/solution.py
@@ -1,20 +1,23 @@
 class Solution:
-    def numDecodings(self, text: str) -> int:
-        # number of ways to split the encoded msg 
-        # such that each item will be 1 <= <= 26 and no leading zeros
-        n = len(text)
+    @lru_cache(maxsize=None)
+    def recursiveWithMemo(self, index, s) -> int:
+        # If you reach the end of the string
+        # Return 1 for success.
+        if index == len(s):
+            return 1
+
+        # If the string starts with a zero, it can't be decoded
+        if s[index] == '0':
+            return 0
+
+        if index == len(s)-1:
+            return 1
         
-        prev = 1 if text[0] != '0' else 0
-        secondPrev = 1 
-        for i in range(2, n+1):
-            cur = 0
-            if text[i-1] != '0':
-                cur = prev
-            
-            if i-2 >= 0 and 10 <= int(text[i-2:i]) <= 26:
-                cur += secondPrev
-            
-            prev, secondPrev = cur, prev
-        return prev
-        
-        
+        answer = self.recursiveWithMemo(index + 1, s)
+        if int(s[index : index + 2]) <= 26:
+            answer += self.recursiveWithMemo(index + 2, s)
+
+        return answer
+
+    def numDecodings(self, s: str) -> int:
+        return self.recursiveWithMemo(0, s)
