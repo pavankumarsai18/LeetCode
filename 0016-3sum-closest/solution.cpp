675f74d74d160c8b6ab6eb53d6febe822fcc6557
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-10-11 17:19:56 +0000

    [LeetCode Sync] Runtime - 445 ms (8.01%), Memory - 16.3 MB (27.86%)

--- 0016-3sum-closest/solution.cpp
+++ 0016-3sum-closest/solution.cpp
@@ -3,33 +3,25 @@ public:
     int threeSumClosest(vector<int>& nums, int target) 
     {
         const int n = nums.size();
-        
         int diff = INT_MAX;
-        int closest_sum = INT_MAX;
         
         sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n && diff != 0; i++)
+        int sum;
+        for(int i = 0; i < n; i++)
         {
-            int lo = i + 1;
-            int hi = n - 1;
-            
+            int lo = i + 1, hi = n - 1;
             while(lo < hi)
             {
-                int sum = nums[i]+nums[lo]+nums[hi];
-                if(abs(target-sum) < abs(diff))
-                {
-                    diff = target - sum;
-                    closest_sum = sum;
-                }
-                if(sum < target)
-                    ++lo;
-                else
-                    --hi;
+                sum = nums[i] + nums[lo] + nums[hi];
+                
+                if(abs(target - sum) < abs(diff)) diff = target - sum;                
+                if(sum < target) lo++;
+                
+                else hi--;
             }
+            if(diff == 0) break;
         }
         
         return target - diff;
-               
     }
 };
