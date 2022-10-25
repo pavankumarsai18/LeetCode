2cb54bd829951b4533c1c7828b8d9a12832c90cb
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-10-25 16:15:20 +0000

    [LeetCode Sync] Runtime - 6 ms (1.04%), Memory - 11.3 MB (100.00%)

--- 1781-check-if-two-string-arrays-are-equivalent/solution.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/solution.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
     {
-        string A, B;
-        A = B = "";
+        int index1, index2;
+        int ptr1, ptr2;
         
-        for(auto & word: word1)
-            A += word;
-        for(auto & word: word2)
-            B += word;
         
-        return A == B;
+        index1 = index2 = ptr1 = ptr2 = 0;
         
+        while(index1 < word1.size() && index2 < word2.size())
+        {
+                   
+            if(word1[index1][ptr1++] != word2[index2][ptr2++]) return false;
+            
+            if(ptr1 == word1[index1].size()){index1++; ptr1 = 0;}
+            if(ptr2 == word2[index2].size()){index2++; ptr2 = 0;}
+                        
+        }
+        
+        return index1 == word1.size() && index2 == word2.size();
     }
 };
