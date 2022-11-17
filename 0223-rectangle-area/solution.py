4e850136ea303dbe76d830ef2c2779a6cb20f0c1
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2022-11-17 15:18:10 +0000

    [LeetCode Sync] Runtime - 113 ms (5.61%), Memory - 13.9 MB (100.00%)

--- 0223-rectangle-area/solution.py
+++ 0223-rectangle-area/solution.py
@@ -1,36 +1,21 @@
 class Solution:
-    def axisIntersection(self, ax1:int, ax2: int,bx1:int, bx2:int):
-        
-        # No intersection
-        if ax2 <= bx1:
+                
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:    
+        # print(f"area1 A {(ax2 - ax1)*(ay2 - ay1)} area2 B {(bx2 - bx1)*(by2 - by1)} intersection {self.intersects(ax1, ax2, bx1, bx2)*self.intersects(ay1, ay2, by1, by2)}")
+        def intersects(p1a:int, p1b:int, p2a:int, p2b:int) -> int:
+            # p1_a p2_a p1_b p2_b
+            if p1a <= p2a <= p1b <= p2b:
+                return p1b - p2a
+            # p2a p1a p2b p1b
+            elif p2a <= p1a <= p2b <= p1b:
+                return p2b - p1a
+            # p1a p2a p2b p1b
+            elif p1a <= p2a <= p2b <= p1b:
+                return p2b - p2a
+            # p2a p1a p1b p2b
+            elif p2a <= p1a <= p1b <= p2b:
+                return p1b - p1a
             return 0
-        if bx2 <= ax1:
-            return 0
-        
-        if ax1 <= bx1 <= bx2 <= ax2:
-            return bx2 - bx1
-        if ax1 <= bx1 <= ax2 <= bx2:
-            return ax2-bx1
-        if bx1 <= ax1 <= bx2 <= ax2:
-            return bx2 - ax1
-        return ax2 - ax1
-    
-    def intersection(self, ax1:int, ay1:int, ax2: int, ay2:int, bx1:int, by1:int, bx2:int, by2:int):
-        return self.axisIntersection(ax1, ax2, bx1, bx2)*self.axisIntersection(ay1, ay2, by1, by2)
-            
-    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        total_area = (ax2-ax1)*(ay2-ay1) + (bx2 - bx1)*(by2-by1)
-        
-        # No intersection
-        
-        # A intersects B
-        
-        # B in A
-        
-        # B intersects A
-        
-        # A in B
+        return (ax2 - ax1)*(ay2 - ay1) + (bx2 - bx1)*(by2 - by1) -(intersects(ax1, ax2, bx1, bx2)*intersects(ay1, ay2, by1, by2))
         
         
-        total_area -= self.intersection(ax1,ay1,ax2,ay2,bx1,by1,bx2,by2)
-        return total_area
