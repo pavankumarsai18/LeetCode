6461557f944de2d1e0d4dc56ec8f8f2810385f38
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-08-29 19:08:57 +0000

    [LeetCode Sync] Runtime - 102 ms (7.90%), Memory - 71.3 MB (74.27%)

--- 0217-contains-duplicate/solution.cpp
+++ 0217-contains-duplicate/solution.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
-        unordered_map<int, int> numCount;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            numCount[nums[i]]++;
-        }
-        return numCount.size() != nums.size();
-        
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> unique (nums.begin(), nums.end());
+
+        return unique.size() < nums.size();
     }
 };
