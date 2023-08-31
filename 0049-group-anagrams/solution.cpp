7944088e04fb833ce3ce9b706db81e012af3b973
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-08-31 02:53:30 +0000

    [LeetCode Sync] Runtime - 119 ms (5.03%), Memory - 27.8 MB (21.75%)

--- 0049-group-anagrams/solution.cpp
+++ 0049-group-anagrams/solution.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    string hashAnagram(string & str)
-    {
-        const int numChars = 26;
-        int charCount[numChars] = {0};
-        // Get the char count
-        for(int i = 0; i < str.size(); i++)
-        {
-            charCount[str[i]-'a']++;
+    string getCharCountString(const string & str) {
+        vector<int> charCount(26, 0);
+        for(int i = 0; i < str.size(); ++i) {
+            charCount[str[i] - 'a']++;
         }
 
+        string ans = "";
 
-        string result = "";
-        // convert it into a string
-        for(int i = 0; i < numChars; i++)
-        {
-            result += to_string(charCount[i]) + ',';
+        for(int i = 0; i < charCount.size(); ++i) {
+            ans += to_string(charCount[i]) + " ";
         }
 
-        return result;
-    }
-
-    vector<vector<string>> groupAnagrams(vector<string>& words) 
-    {
-        unordered_map<string, vector<string>> map;
-        
-        
-        for(int i = 0; i < words.size(); i++)
-        {
-            string hashValue = hashAnagram(words[i]);
-            map[hashValue].push_back(words[i]);
-        }
-        
-        vector<vector<string>> ans;
-        
-        for(auto & [hashVal, anagrams]: map)
-            ans.push_back(anagrams);
         return ans;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        
+        unordered_map<string, vector<string>> anagramMap;
+
+        for(int i = 0; i < strs.size(); i++) {
+            string charCountStr = getCharCountString(strs[i]); 
+            if(anagramMap.find(charCountStr) == anagramMap.end()) {
+                anagramMap[charCountStr] = {};
+            }
+            anagramMap[charCountStr].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto itr = anagramMap.begin(); itr != anagramMap.end(); itr++) {
+            ans.push_back(itr->second);
+        }
+
+        return ans;
     }
 };
