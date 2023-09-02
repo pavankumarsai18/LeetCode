c93f3d5bbff4b067b42935ef26a57b47df82cd7e
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-09-02 23:42:47 +0000

    [LeetCode Sync] Runtime - 16 ms (9.30%), Memory - 19.1 MB (100.00%)

--- 0283-move-zeroes/solution.cpp
+++ 0283-move-zeroes/solution.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) 
-    {
-        // int count = 0;
-        // for(int i = 0; i < nums.size(); i++)
-        //     if(nums[i] == 0)
-        //         count++;
-        
-        // 0 1 0 3 12
-        
-        // 1 0 0 3 12
-        // 1 3 12 0 0
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     if(nums[i] != 0)
-        //     {
-        //         int j = i;
-        //         while(j >= 1 && nums[j] != 0 && nums[j - 1] == 0)
-        //         {
-        //             std::swap(nums[j], nums[j - 1]);
-        //             j --;
-        //         }
-        //     }
-        // }
-        
-        int last_zero = 0;
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] != 0)
-                nums[last_zero++] = nums[i];
+    void moveZeroes(vector<int>& nums) {
+        const int n = nums.size();
+        int readIdx, writeIdx;
+        readIdx = writeIdx = 0;
+
+        for(; readIdx < n; ++readIdx) {
+            if (nums[readIdx] == 0) continue;
+            nums[writeIdx++] = nums[readIdx];
         }
-        for(int i = last_zero; i < nums.size(); i++)
-            nums[i] = 0;
-        
-        
+
+        for (; writeIdx < n; ++writeIdx) {
+            nums[writeIdx] = 0;
+        }
     }
 };
