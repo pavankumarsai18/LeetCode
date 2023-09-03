823dbdd9bb30dbdb546b03225198050d2f6c7b87
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-09-03 16:14:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6 MB (100.00%)

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/solution.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/solution.cpp
@@ -1,65 +1,29 @@
 class Solution {
-private:
-    string toString(int s)
-    {
-        string result = "";
-        
-        while(s > 0)
-        {
-            string b = "0";
-            
-            if(s&1)
-            {
-                b = "1";
-            }
-            
-            result = b + result;
-            s>>=1;
-            
-        }
-       
-        return result;
-    }
 public:
-    int minFlips(int a, int b, int c)
-    {
-        string A = toString(a);
-        string B = toString(b);
-        string C = toString(c);
-        
-        int numflips = 0;
-        
-        int sz = (A.size() > B.size())? A.size():B.size();
-        sz = (sz > C.size())? sz:C.size();
-        
-        while(A.size() != sz){A = "0"+A;}
-        while(B.size() != sz){B = "0"+B;}
-        while(C.size() != sz){C = "0"+C;}
-        
-        for(int i = 0; i < sz; i++)
-        {
-            if(C[i] == '0')
-            {
-                if(A[i] == '1' && B[i] == '1')
-                {
-                    numflips += 2;
+    int minFlips(int a, int b, int c) {
+        int numFlips = 0;
+
+        while (c | a | b) {
+            bool cBit = (c & 1);
+            bool aBit = (a & 1), bBit = (b & 1);
+
+            if ((aBit | bBit) ^ cBit) {
+                if (cBit && !(aBit & bBit)) {
+                    numFlips++;
+                } else if (!cBit) {
+                    if (aBit ^ bBit) {
+                        numFlips++;
+                    } else {
+                        numFlips += 2;
+                    }
                 }
-                else if(A[i] == '0' && B[i] == '0'){;}
-                else
-                {
-                    numflips += 1;
-                }
-            }
-            else
-            {
-                if(A[i] == '1' || B[i] == '1')
-                {;}
-                else{numflips += 1;}
             }
 
+            c >>= 1;
+            a >>= 1;
+            b >>= 1;
         }
-        
-        return numflips;
-        
+
+        return numFlips;
     }
 };
