9283ad3ac63332f5da0e2bd385f1ab6ebe771ebe
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-09-04 15:22:13 +0000

    [LeetCode Sync] Runtime - 6 ms (5.60%), Memory - 7.9 MB (100.00%)

--- 0338-counting-bits/solution.cpp
+++ 0338-counting-bits/solution.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    vector<int> countBits(int n) 
-    {
-        if(n == 0)
-        {
-            return {0};
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1, 0);
+
+        for (int num = 1; num <= n; ++num) {
+            ans[num] = ans[num>>1] + (num&1);
         }
-        
-        vector<int> result = {0,1};
-        
-        while(result.size() < (n+1))
-        {
-            int curSize = result.size();
-            int i       = 0;
-            while(i < curSize && result.size() < (n+1))
-            {
-                result.push_back(result[i] + 1);
-                i++;
-            }
-        }
-        return result;
+
+        return ans;
     }
 };
