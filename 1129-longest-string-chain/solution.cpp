1e9a11e5e1600ed819e2d0a34b83c8632ab5154b
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-09-24 22:40:50 +0000

    [LeetCode Sync] Runtime - 65 ms (49.64%), Memory - 20.4 MB (60.58%)

--- 1129-longest-string-chain/solution.cpp
+++ 1129-longest-string-chain/solution.cpp
@@ -1,63 +1,43 @@
-class Solution 
-{
-private:
-    vector<int> score;
-    vector<vector<int>> adjList;
-    
+class Solution {
 public:
-    int findLongestChain(int word)
-    {
-        if(score[word] != 0)
-            return score[word];
-        
-        int curScore = 1;
-        for(int neighbor: adjList[word])
-        {
-            curScore = max(curScore, findLongestChain(neighbor) +1);
-        }
-        score[word] = curScore;
-        
-        return curScore;
-    }
-    int longestStrChain(vector<string>& words) 
-    {
-        const int n = words.size();
-        unordered_map<string, int> wordIndex;
-        
-        score = vector<int>(n, 0);
-        adjList = vector<vector<int>>(n, vector<int>());
-        
-        for(int i = 0; i < n; i++)
-        {
-            wordIndex[words[i]] = i;
-        }
-        
-        
-        for(int i = 0; i < n; i++)
-        {
-            string word = words[i];
+    int findLongestChain(const string & word, const unordered_set<string> & wordSet, unordered_map<string, int> & memo) {
+
+        if (memo.find(word) != memo.end()) 
+            return memo[word];
+
+        int ans = 1;
+
+        const int len = word.size();
+
+        for (int i = 0; i < len; ++i) {
+            string newString = word.substr(0,i) + word.substr(i+1);
             
-            const int wordLen = words[i].size();
-            
-            for(int removeIndex = 0; removeIndex < wordLen; removeIndex++)
-            {
-                string newWord = word.substr(0, removeIndex) + word.substr(removeIndex+1);
-                    
-                auto itr = wordIndex.find(newWord);
-                if(itr != wordIndex.end())
-                {
-                    adjList[i].push_back(itr->second);
-                }
+            if(wordSet.find(newString) != wordSet.end()) {
+                int chainLength = findLongestChain(newString, wordSet, memo);
+                chainLength++;
+                ans = max(ans, chainLength);
             }
-        }
         
-        int ans = 0;
-        for(int word = 0; word < n; word++)
-        {
-            ans = max(ans, findLongestChain(word));
         }
-        
+
+        memo[word] = ans;
         return ans;
         
+    }
+    int longestStrChain(vector<string>& words) {
+        // sort the words based on size
+        // create a graph and find the maximum depth
+        
+        unordered_set<string> wordSet(words.begin(), words.end());
+
+        unordered_map<string, int> memo;
+        int maxChain = 0;
+        for (const auto & word: words) {
+            maxChain = max(maxChain, findLongestChain(word, wordSet, memo));
+        }
+    
+
+        return maxChain;
+
     }
 };
