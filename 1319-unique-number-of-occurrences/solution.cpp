774fc5678975a36f36643e383765fcc807ac390e
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-09-25 02:26:13 +0000

    [LeetCode Sync] Runtime - 5 ms (1.55%), Memory - 8.6 MB (100.00%)

--- 1319-unique-number-of-occurrences/solution.cpp
+++ 1319-unique-number-of-occurrences/solution.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        
-        map<int, int> num_count;
-        for(size_t i = 0; i < arr.size(); i++)
-        {
-            auto itr = num_count.find(arr[i]);
-            if(itr == num_count.end())
-            {
-                num_count[arr[i]] = 1;
-            }
-            else
-            {
-                num_count[arr[i]] += 1;
-            }
+        unordered_map<int, int> numCount;
+        for(int i = 0; i < arr.size(); ++i) {
+            numCount[arr[i]]++;
         }
-        
-        map<int, int> count_count;
-        for(auto itr = num_count.begin(); itr != num_count.end(); itr++)
-        {
-            auto itr_ = count_count.find(itr->second);
-            if(itr_ == count_count.end())
-            {
-                count_count[itr->second] = 1;
-            }
-            else
-            {
-                count_count[itr->second] += 1;   
-            }
-            
-        }
-        
-        //
-        for(auto itr = count_count.begin(); itr != count_count.end(); itr++)
-        {
-            if(itr->second > 1)
-                return false;
-        }
-        
-        return true;
+
+        unordered_set<int> uniqueCount;
+        for (auto & [key, value] : numCount)
+            uniqueCount.insert(value);
+
+        return uniqueCount.size() == numCount.size();
     }
 };
