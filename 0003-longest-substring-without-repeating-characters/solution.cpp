9a264a3106b29cf363844402b2b34fba5bc6262a
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-10-21 20:43:34 +0000

    [LeetCode Sync] Runtime - 11 ms (52.69%), Memory - 8.8 MB (99.98%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) 
-    {
+    int lengthOfLongestSubstring(string s) {
+        int lo, hi;
+        int longestSubStr = 0;
         const int n = s.size();
         unordered_map<char, int> charCount;
-        int left, ans;
-        char ch;
-        left = ans = 0;
-        
-        for(int right = 0; right < n; right++)
-        {
-            ch = s[right];
-            charCount[ch]++;
-            
-            if(charCount.find(ch) == charCount.end()) ans = max(ans, right - left + 1);
-            
-            while(left <= right && charCount[ch] > 1)
-            {
-                charCount[s[left]]--;
-                if(charCount[s[left]] == 0) charCount.erase(s[left]);
-                left++;
+
+        lo = 0;
+        hi = 0;
+
+
+        for (; hi < n; hi++) {
+            charCount[s[hi] - 'a']++;
+            while(lo <= hi && charCount[s[hi] - 'a'] != 1) {
+                charCount[s[lo] - 'a']--;
+                lo++;
             }
             
-            ans = max(ans, right - left + 1);
+            longestSubStr = max(longestSubStr, hi - lo + 1);
+ 
         }
-        
-        return ans;
+
+        return longestSubStr;
     }
 };
