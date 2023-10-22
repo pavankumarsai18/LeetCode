4f23da1a7127ce017487e33ebbf729fef3f2cad2
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-10-22 14:33:09 +0000

    [LeetCode Sync] Runtime - 17 ms (5.46%), Memory - 13.9 MB (100.00%)

--- 0347-top-k-frequent-elements/solution.cpp
+++ 0347-top-k-frequent-elements/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) 
-    {
-        unordered_map<int, int> num_count;
-        
-        for(int i = 0; i < nums.size(); i++)
-        {            
-            num_count[nums[i]]++;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> numCount;
+        vector<int> frequencies;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (numCount.find(nums[i]) == numCount.end()) frequencies.push_back(nums[i]);
+            numCount[nums[i]]++;
         }
-        
-        auto cmp = [&num_count](int & a, int & b){return num_count[a] < num_count[b];};
-        
-        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
-        for(auto [num, count]: num_count)
-        {
-            pq.push(num);
+
+
+        auto cmp = [&numCount](auto lhs, auto rhs){ return numCount[lhs] < numCount[rhs];};
+
+
+        std::make_heap (frequencies.begin(),frequencies.end(), cmp);
+
+        vector<int> ans;
+        while(ans.size() != k) {
+            std::pop_heap(frequencies.begin(), frequencies.end(), cmp);
+            ans.push_back(frequencies.back());
+            frequencies.pop_back();
         }
-        
-        vector<int> result;
-        while(result.size() < k && pq.size() > 0)
-        {
-            result.push_back(pq.top());
-            pq.pop();
-        }
-        
-        return result;
-        
+
+        return ans;
     }
 };
