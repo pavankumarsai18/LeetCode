bbec4bf13b4f5d5b2af0a6f3eaffc957bd34c64b
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-10-24 17:54:01 +0000

    [LeetCode Sync] Runtime - 94 ms (22.36%), Memory - 24.4 MB (100.00%)

--- 0015-3sum/solution.cpp
+++ 0015-3sum/solution.cpp
@@ -1,55 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> twoSum(vector<int> & nums, int excludedIndex, int target)
-    {
+    vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int lo = excludedIndex + 1;
-        int hi = nums.size() - 1;
-        int sum;
-        while(lo < hi)
-        {
-            sum = nums[lo] + nums[hi];
-            if(sum > target)
-                hi--;
-            else if(sum < target)
-                lo++;
-            else
-            {
-                ans.push_back({nums[lo], nums[hi]});
-                lo++;
-                hi--;
-                while(lo < hi && nums[lo] == nums[lo - 1])
-                {
-                    lo++;
-                }
-            }
-            
-        }
-        
-        return ans;
-        
-    }
-    vector<vector<int>> threeSum(vector<int>& nums) 
-    {
-        vector<vector<int>> ans;
-        unordered_set<int> seen;
-        
+
         sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] > 0) break;
+
+        const int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
             
-            if(i == 0 || nums[i-1] != nums[i])
-            {
-                int target = -nums[i];
-                for(auto & pos: twoSum(nums,i, target))
-                {
-                    pos.push_back(nums[i]);
-                    ans.push_back(pos);
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left - 1] == nums[left]) {
+                        left++;
+                    }
                 }
             }
         }
+
         return ans;
+
     }
 };
