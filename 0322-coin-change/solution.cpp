7cbc830a52f89e6041f41a9b6408dade520d2368
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-12-15 01:15:10 +0000

    [LeetCode Sync] Runtime - 331 ms (5.12%), Memory - 65.6 MB (6.03%)

--- 0322-coin-change/solution.cpp
+++ 0322-coin-change/solution.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-    int minimumCoins(vector<int>& coins, int amount, unordered_map<int, int> & cache)
-    {
-        // cout<<amount<<endl;
-        // for(auto & item: cache)
-        //     cout<<item.first<<": "<<item.second<<endl;
-        // cout<<"--------\n";
-
-        if(cache.find(amount) != cache.end())
-            return cache[amount];
-            
-        if(amount < 0)
-        {
-            cache[amount] = -1;
-            return -1;
-        }
+    int helper(vector<int>& coins, int amount, unordered_map<int, int> & memo) {
+        if (amount == 0) return 0;
+        else if (amount < 0) return -1;
         
+        if (memo.find(amount) != memo.end()) return memo[amount];
         
-        int numCoins = INT_MAX;
-        for(int i = 0; i < coins.size(); i++)
-        {
-            int coinsRequired = minimumCoins(coins, amount-coins[i], cache);
-            // cout<<coins[i]<<" "<<coinsRequired<<endl;;
-
-            if(coinsRequired >= 0 &&coinsRequired < INT_MAX)
-            {
-                numCoins = min(numCoins, coinsRequired + 1);
-                cache[amount] = numCoins;
+        int ans = INT_MAX;
+        for (auto coin : coins) {
+            int numCoinsRequired = helper(coins, amount - coin, memo);
+            if (numCoinsRequired == -1) {
+                continue;
             }
-        }
-        
-        if(numCoins == INT_MAX)
-        {
-           cache[amount] = -1;
-        }
-        
-        return cache[amount];
-        
+            ans = min(ans,  numCoinsRequired + 1);
+        }        
+
+        if (ans == INT_MAX) ans = -1;
+        memo[amount] = ans;
+        return ans;
+
     }
-    
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        unordered_map<int, int> cache;
-        for(auto coin:coins)
-            cache[coin] = 1;
-        cache[0] = 0;
-        return minimumCoins(coins, amount, cache);
-    
+    int coinChange(vector<int>& coins, int amount) {
+        unordered_map<int, int> memo;
+        return helper(coins, amount, memo);
     }
 };
