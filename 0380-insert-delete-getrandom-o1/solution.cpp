1a228a37ace57165e4ea591a957de56b642d8a30
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2023-12-15 01:40:52 +0000

    [LeetCode Sync] Runtime - 177 ms (5.01%), Memory - 97.4 MB (100.00%)

--- 0380-insert-delete-getrandom-o1/solution.cpp
+++ 0380-insert-delete-getrandom-o1/solution.cpp
@@ -1,130 +1,45 @@
 class RandomizedSet {
 private:
-    int capacity;
-    int numDeleted;
-    int size;
-    unordered_map<int, pair<int, bool>> num_index;
-    unordered_map<int, int> index_num;
-    unordered_set<int> s;
-    
+    vector<int> numList;
+    unordered_map<int, int> numIndex;
 public:
-    RandomizedSet() 
-    {
-        srand(time(0));
-        
-        num_index = unordered_map<int, pair<int, bool>>();
-        index_num = unordered_map<int, int>();
-        s = unordered_set<int>();
-        
-        size = 0;
-        capacity = 1;
-        numDeleted = 0;
+    RandomizedSet() {
     }
     
-    bool insert(int val) 
-    {
-        // cout<<"In insert "<<endl;
+    bool insert(int val) {
+        bool found = numIndex.find(val) != numIndex.end();
 
-        if(s.find(val) != s.end())
-        {
-            return false;
-        }
+        numList.push_back(val);
+        numIndex[val] = numList.size() - 1;
         
-        
-        s.insert(val);
-        index_num[size] = val;
-        num_index[val] = {size, false}; // is deleted = false
-        size++;
-        
-        if(size >= capacity)
-        {
-            int index = 0;
-            num_index.clear();
-            index_num.clear();
-            for(auto itr = s.begin(); itr != s.end(); itr++)
-            {
-                num_index[*itr] = {index, false}; // is delteed = false
-                index_num[index] = *itr;
-                index++;
-            }
-            size = num_index.size();
-            capacity *= 2;
-        }
-        
-//         for(auto itr: num_index)
-//         {
-//             cout<<itr.first<<"->"<<itr.second.first<<","<<itr.second.second<<endl;
-//         }
-//         cout<<"-----\n";
-
-//         for(auto itr: index_num)
-//         {
-//             cout<<itr.first<<"->"<<itr.second<<endl;
-//         }
-        
-//         cout<<"size = "<<size<<endl;
-
-        return true;
-        
+        return !found;
     }
     
-    bool remove(int val) 
-    {
-        if(s.find(val) == s.end())
-        {
-            return false;
-        }
-        
-        pair<int, bool> p = num_index[val];
-        num_index[val] = {p.first, true}; // set deleted to true
-        s.erase(val);
-        numDeleted++;
-        size--;
-        if(numDeleted >= capacity/2)
-        {
-            int index = 0;
-            num_index.clear();
-            index_num.clear();
-            for(auto itr = s.begin(); itr != s.end(); itr++)
-            {
-                num_index[*itr] = {index, false}; // is deleted = false
-                index_num[index] = *itr;
-                index++;
-            }
-            numDeleted = 0;
-            capacity = capacity/2; 
-            size = num_index.size();
-        }
-        
-        // cout<<"In remove "<<endl;
-        // for(auto itr: num_index)
-        // {
-        //     cout<<itr.first<<"->"<<itr.second.first<<","<<itr.second.second<<endl;
-        // }
-//         cout<<"-----\n";
-//         for(auto itr: index_num)
-//         {
-//             cout<<itr.first<<"->"<<itr.second<<endl;
-//         }
-        
-        // cout<<"size = "<<size<<endl;
-        return true;
+    bool remove(int val) {
+
+        bool found = numIndex.find(val) != numIndex.end();
+
+        if (!found) return found;
 
         
-        
+        int index = numIndex[val];
+        int lastElement = numList.back();
+        numIndex[lastElement] = index;
+
+        swap(numList[index], numList[numList.size()-1]);
+
+        numList.pop_back();
+        numIndex.erase(val);
+
+        return found;
     }
     
-    int getRandom() 
-    {
+    int getRandom() {
+
+        int randIndex = rand() % numList.size();
+
+        return numList[randIndex];
         
-        
-        int index = rand()%index_num.size();
-        while(num_index[index_num[index]].second == true)
-        {
-            index = rand()%index_num.size();
-        }
-        
-        return index_num[index];
     }
 };
 
