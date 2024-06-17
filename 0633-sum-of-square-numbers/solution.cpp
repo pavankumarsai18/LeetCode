96d1bf8fd88a1e02f28c5a34dcae5d61227953f2
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-06-17 22:52:08 +0000

    [LeetCode Sync] Runtime - 48 ms (5.26%), Memory - 7.1 MB (100.00%)

--- 0633-sum-of-square-numbers/solution.cpp
+++ 0633-sum-of-square-numbers/solution.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+        
     bool judgeSquareSum(int c) {
-        
-        //a^2 + b^2 = c
-        // (a/c)* a + (b/c)*b = 1
-        // a/c*a = 1 - (b/c)*b
-        // sqrt(c) - 1 >= a >= 1
-        // sqrt(c) - 1 >= b >= 1
-        
-        // 16
-        // 1 4 9 16
-        
-        int max = sqrt(c);
-        int min = 0;
-        for(int i = min; i < max + 1; i++)
-        {
-            // a = i;
-            // b = sqrt(c - i*i)
-            int b = sqrt(c - i * i);
-            
-            if(b*b + i*i == c)
-                return true;
+        for (long a = 0; a*a <= c; ++a) {
+            int b = c - (int)a*a;
+
+            bool is_square = false;
+            int start = 0, end = b, target = b;
+            while (start <= end) {
+                const long mid = start + ((end - start) >> 1);
+                const long mid_sq = mid*mid;
+
+                if (mid_sq == target) {
+                    return true;
+                } else if (mid_sq < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
         }
-        
         return false;
     }
 };
