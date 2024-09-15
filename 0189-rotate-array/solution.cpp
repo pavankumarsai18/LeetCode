6fdc63d62c3be181c7a69b6f02bc8bec54f4d29f
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-15 23:51:11 +0000

    [LeetCode Sync] Runtime - 31 ms (2.53%), Memory - 27.7 MB (100.00%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -1,58 +1,15 @@
 class Solution {
-private:
-    void rotateOnce(vector<int>& nums)
-    {
-        int last = nums[nums.size() - 1];
-        int temp = nums[0];
-        for(int i = 1; i < nums.size(); i++)
-        {
-            int x = nums[i];
-            nums[i] = temp;
-            temp = x;
-        }
-        nums[0] = last;
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
     }
 
-public:
-    void rotate(vector<int>& nums, int k) 
-    {
-        if(nums.size() == 0 || nums.size() == 1)
-            return;
-        // vector<int> Copy = nums;
-        // int shiftAmt = k;
-        // // i --> (i + k) %n
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     int newIndex = (i + shiftAmt)%nums.size();
-        //     nums[newIndex] = Copy[i];
-        // }
-        
-        
-        int startIndex, nextIndex, numShifted;
-        int cur = 0;
-        startIndex = numShifted = 0;
-        int temp = nums[0];
-        
-        while(numShifted != nums.size())
-        {
-            cur = startIndex;
-            temp = nums[startIndex];
-            
-            do
-            {
-                nextIndex = (cur + k)%nums.size();
-                
-                swap(temp, nums[nextIndex]);
-                
-                cur = nextIndex;
-                numShifted++;
-                
-
-            }while(cur != startIndex);
-            
-            startIndex++;
+    void reverse(vector<int>& nums, int left, int right) {
+        while (left < right) {
+            std::swap(nums[left++], nums[right--]);
         }
-        
-        
     }
 };
