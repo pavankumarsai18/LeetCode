a89023412cd963fb878f28c6b8d618611efcdae0
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-15 22:31:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.7 MB (100.00%)

--- 0027-remove-element/solution.cpp
+++ 0027-remove-element/solution.cpp
@@ -1,51 +1,19 @@
 class Solution {
-private:
-    void printArr(vector<int>& nums)
-    {
-        for(auto & n: nums)
-            cout<<n<<" ";
-        cout<<endl;
-    }
 public:
     int removeElement(vector<int>& nums, int val) {
-
-        int result = 0;
-        int endIndex = nums.size() - 1;
-        int curIndex = 0;
- 
-        for(; curIndex <= endIndex; curIndex++)
-        {
-            while(endIndex >= 0 && nums[endIndex] == val)
-            {
-                result--;
-                endIndex--;    
+        int numValOccurances = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == val) {
+                numValOccurances++;
+            } else {
+                nums[i - numValOccurances] = nums[i];
             }
-            if(endIndex < 0)
-            {
-                break;
-            }
-            
-            if(nums[curIndex] == val)
-            {
-                result --;
-                swap(nums[curIndex], nums[endIndex]);
-                endIndex--;
-            }
-            while(endIndex >= 0 && nums[endIndex] == val)
-            {
-                result--;
-                endIndex--;    
-            }
-            if(endIndex < 0)
-            {
-                break;
-            }
-            // cout<<"curIndex "<<curIndex<<endl;
-            // cout<<"endIndex "<<endIndex<<endl;
-            // printArr(nums);
         }
-        result += nums.size();
-        return result;
-        
+
+        for (int i = nums.size() - numValOccurances; i < nums.size(); ++i) {
+            nums[i] = val;
+        }
+
+        return nums.size() - numValOccurances;
     }
 };
