82d6bd5acfc6e9bc9399a506117889a9665f8fa8
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-15 22:37:14 +0000

    [LeetCode Sync] Runtime - 16 ms (6.89%), Memory - 21.3 MB (100.00%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,55 +1,24 @@
 class Solution {
-private:
-    void printArr(vector<int>& nums)
-    {
-        for(auto n: nums)
-        {
-            cout<<n<<" ";
-        }
-        cout<<endl;
-    }
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 0 || nums.size() == 1)
-            return static_cast<int>(nums.size());
-        
-        int startIndex = 0;
-        int endIndex = 0;
-        int maxValue = nums[nums.size() - 1];
-        
-        while(endIndex < nums.size() && startIndex < nums.size())
-        {
-            // printArr(nums);
-            int value = nums[startIndex];
-            // cout<<"value "<<value<<endl;
-            // cout<<"startIndex "<<startIndex<<endl;
-            while(endIndex < nums.size() && value == nums[endIndex])
-            {
-                endIndex++;
+        int numDuplicate = 0;
+        int curVal = nums[0];
+        int idx = 0;
+        while (idx < nums.size()) {
+            curVal = nums[idx];
+            while (idx + 1 < nums.size() && curVal == nums[idx + 1]) {
+                idx++;
+                numDuplicate++;
             }
-            // cout<<"endIndex "<<endIndex<<endl;
-            
-            if(endIndex < nums.size() && (startIndex + 1 < nums.size()))
-            {
-                if(nums[startIndex + 1] != nums[endIndex]){
-                    swap(nums[startIndex+1], nums[endIndex]);
-                    endIndex++;
-                }
+            if (idx < nums.size()) {
+                nums[idx - numDuplicate] = nums[idx];
             }
-            startIndex++;
+            idx++;
+      
         }
-       
-        int result = 0;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            result++;
-            if(nums[i] == maxValue)
-            {
-                break;
-            }
-        }
-        return result;
-        
-        
+
+        return nums.size() - numDuplicate;
+
+
     }
 };
