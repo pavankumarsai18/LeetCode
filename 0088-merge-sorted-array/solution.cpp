f74321e02bebdc5a42d2e79a4b8ea75f98c075f0
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-15 20:59:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.1 MB (99.96%)

--- 0088-merge-sorted-array/solution.cpp
+++ 0088-merge-sorted-array/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx1, idx2, idx;
-        idx1 = m - 1;
-        idx2 = n - 1;
+        int read_idx1 = m - 1, read_idx2 = n - 1;
+        int write_idx = m + n - 1;
 
-        int writeIdx = m + n - 1;
-        while (writeIdx >= 0) {
-            if (idx1 < 0 && idx2 >= 0) {
-                nums1[writeIdx--] = nums2[idx2--];
-            } else if (idx2 < 0) {
-                nums1[writeIdx--] = nums1[idx1--];
-            } else {
-                if (nums1[idx1] > nums2[idx2]){
-                    nums1[writeIdx--] = nums1[idx1--];
+        while (write_idx >= 0) {
+            if (read_idx1 >= 0 && read_idx2 >= 0) {
+                if (nums1[read_idx1] >= nums2[read_idx2]) {
+                    nums1[write_idx--] = nums1[read_idx1--];
                 } else {
-                    nums1[writeIdx--] = nums2[idx2--];
+                    nums1[write_idx--] = nums2[read_idx2--];
                 }
+            } else if (read_idx1 == -1 && read_idx2 >= 0) {
+                nums1[write_idx--] = nums2[read_idx2--];
+            } else {
+                nums1[write_idx--] = nums1[read_idx1--];
             }
         }
     }
