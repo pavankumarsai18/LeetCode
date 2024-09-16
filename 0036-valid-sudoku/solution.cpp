b27ac80ce70af4041adbcc0568338627a46bcfa4
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-16 06:02:14 +0000

    [LeetCode Sync] Runtime - 17 ms (5.07%), Memory - 24.1 MB (49.89%)

--- 0036-valid-sudoku/solution.cpp
+++ 0036-valid-sudoku/solution.cpp
@@ -1,54 +1,27 @@
 class Solution {
 public:
-    bool atmostOne(vector<int>& arr)
-    {
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> rowSet(9, vector<bool>(10, false));
+        vector<vector<bool>> colSet(9, vector<bool>(10, false));
+        vector<vector<bool>> subBoardSet(9, vector<bool>(10, false));
 
-        for(auto & item: arr)
-        {
-            if(item  < 1)
-                return false;
-        }
 
-        
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) 
-    {
-        
-        vector<int> subBoards(9, 0);
-        vector<int> cols(9, 0);
-        vector<int> rows(9,0);
-        
-        for(int i = 0; i < 9; i++)
-        {
-            for(int j = 0; j < 9; j++)
-            {
-                int subBoardNumber = (j/3) + (i/3)*3;
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[i].size(); ++j) {
+                if (board[i][j] == '.') continue;
+
+                int num = board[i][j] - '0';
+
+                int subBoardIdx = (i/3)*3 + (j/3);
                 
-                if(board[i][j] == '.')
-                    continue;
-                
-                int val = 1 << (board[i][j] - '1');
-                
-                if((rows[i] & val) > 0)
-                {
+                if (rowSet[i][num] || colSet[j][num] || subBoardSet[subBoardIdx][num]) {
                     return false;
                 }
-                rows[i] |= val;
-                
-                if((cols[j] & val) > 0)
-                    return false;
-                cols[j] |= val;
-                
-                if((subBoards[subBoardNumber] & val) > 0)
-                    return false;
-                subBoards[subBoardNumber] |= val;
+
+                rowSet[i][num] = colSet[j][num] = subBoardSet[subBoardIdx][num] = true;
             }
         }
-        
+
         return true;
-        
-        
-        
     }
 };
