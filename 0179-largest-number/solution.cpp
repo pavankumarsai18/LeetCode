bdfaec2253ef26ab74b1829d6dcb64723a169c86
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-09-18 14:48:18 +0000

    [LeetCode Sync] Runtime - 9 ms (9.02%), Memory - 15.6 MB (87.70%)

--- 0179-largest-number/solution.cpp
+++ 0179-largest-number/solution.cpp
@@ -1,61 +1,31 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) 
-    {
-        
-        vector<string> strNums;
-        
-        bool allZero = false;
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            string number = to_string(nums[i]);
-            strNums.push_back(number);
+    string removeLeadingZeros(string & number) {
+        if (number.size() == 1) return number;
+
+        int lastleadingZeroIdx = -1;
+        int curIdx = 0;
+        while (curIdx < number.size() && number[curIdx] == '0') {
+            lastleadingZeroIdx = curIdx;
+            curIdx++;
         }
-        
-        auto comp = [](string & a, string & b)
-        {
-            string comb1 = a+b;
-            string comb2 = b+a;
-            
-            int index1, index2;
-            index1 = index2 = 0;
-            
-            const int n = comb1.size();
-            
-            while(index1 < n && index2 < n)
-            {
-                if(comb1[index1] == comb2[index2])
-                {
-                    index1++;
-                    index2++;
-                }
-                else
-                {
-                    return comb1[index1] < comb2[index2];
-                }
-            }
-            
-            return comb1 < comb2;
-        };
-        
-        sort(strNums.begin(), strNums.end(), comp);
-        
-        string result = "";
-        
-        
-        
-        for(int i = strNums.size()-1; i >= 0; i--)
-        {
-            result += strNums[i];
+
+        if (lastleadingZeroIdx != -1) return number.substr(lastleadingZeroIdx);
+        return number;
+    }
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [] (int & num1, int & num2) {
+            string num1_str = std::to_string(num1);
+            string num2_str = std::to_string(num2);
+
+            return num1_str + num2_str > num2_str + num1_str;
+        });
+
+        string largestNumberPossible = "";
+        for (int i = 0; i < nums.size(); ++i) {
+            largestNumberPossible += std::to_string(nums[i]);
         }
-        
-        if(result[0] == '0')
-        {
-            return "0";
-        }
-        
-        return result;
-        
+
+        return removeLeadingZeros(largestNumberPossible);
     }
 };
