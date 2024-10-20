32cb925a31b715cebc966d08ec735ea75f533b7f
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-10-20 20:03:03 +0000

    [LeetCode Sync] Runtime - 2 ms (77.50%), Memory - 25.8 MB (51.01%)

--- 0680-valid-palindrome-ii/solution.cpp
+++ 0680-valid-palindrome-ii/solution.cpp
@@ -1,63 +1,32 @@
 class Solution {
-private:
-    bool isPalindrome(const string & s)
-    {
-        for(int i = 0 ; i < s.size()/2; i++)
-        {
-            if(s[i] != s[s.size() -1 - i])
-                return false;
-        }
-        return true;
-    }
-    
-    bool isPalindrome(const string & s, const int& index)
-    {
-        int i = 0;
-        int j = s.size() - 1;
-        
-        while(i < j)
-        {
-            if(i == index)
-            {
-                i++;
-                continue;
-            }
-            if(j == index)
-            {
-                j--;
-                continue;
-            }
-            if(i != index || j != index)
-            {
-                if(s[i] != s[j])
-                    return false;
-            }
-            
-            i++;
-            j--;
-
-        }
-        return true;
-    }
 public:
-    bool validPalindrome(string s) 
-    {
-        
-        if(isPalindrome(s))
-            return true;
-        
-        int i = 0;
-        int j = s.size() - 1;
-        
-        while(i < j)
-        {
-            if(s[i] != s[j])
-            {
-                return (isPalindrome(s, i) || isPalindrome(s, j));
+    bool isPalindrome(string & s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
             }
-            i++;j--;
+            left++;
+            right--;
         }
-        
+        return true;
+    }
+
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size()  - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                string skipL = s.substr(left + 1, (right - left));
+                string skipR = s.substr(left, (right - left));
+                return (isPalindrome(skipL) || isPalindrome(skipR));
+            }
+
+            left++;
+            right--;
+        }
+
         return true;
     }
 };
