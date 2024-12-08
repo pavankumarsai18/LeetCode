51eaf431ae29318b1195a9771eddf237ab432c53
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-12-08 19:12:47 +0000

    [LeetCode Sync] Runtime - 174 ms (20.32%), Memory - 137.4 MB (6.88%)

--- 0325-maximum-size-subarray-sum-equals-k/solution.cpp
+++ 0325-maximum-size-subarray-sum-equals-k/solution.cpp
@@ -1,32 +1,28 @@
 class Solution {
-public:
-    int maxSubArrayLen(vector<int>& nums, int k)
-    {
-        long long prefixSum = 0;
-        int longestLength = 0;
-        
-        unordered_map<long long, int> seen;
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            prefixSum += nums[i];
-            
-            if(prefixSum == k)
-            {
-                longestLength = (i+1);
+public:    
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        int length = nums.size();
+        vector<long long> prefix_sum(length + 1, 0);
+        int maxLengthSoFar = 0;
+        unordered_map<long long, int> sum_seen;
+        for (int i = 0; i < nums.size(); i++) {
+            prefix_sum[i+1] = prefix_sum[i] + nums[i];
+            if (sum_seen.find(prefix_sum[i+1]) == sum_seen.end()) {
+                sum_seen[prefix_sum[i+1]] = i + 1;
             }
-            
-            if(seen.find(prefixSum - k) != seen.end())
-            {
-                longestLength = max(longestLength, i - seen[prefixSum-k]);
-            }
-            
-            if(seen.find(prefixSum) == seen.end())
-            {
-                seen[prefixSum] = i;
+            if (prefix_sum[i+1] == k) {
+                maxLengthSoFar = max(maxLengthSoFar, (i+1));
+            } else {
+                long long target_sum = prefix_sum[i+1] - k;
+                
+                if (sum_seen.find(target_sum) != sum_seen.end()) {
+                    maxLengthSoFar = max(maxLengthSoFar, (i + 1 - sum_seen[target_sum]));
+                }
             }
         }
         
-        return longestLength;
+        
+        
+        return maxLengthSoFar;
     }
 };
