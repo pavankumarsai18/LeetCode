964d8ef9026d35c68340ea64fc30bfc3e28a4fff
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-12-30 21:06:30 +0000

    [LeetCode Sync] Runtime - 8 ms (10.46%), Memory - 20 MB (13.66%)

--- 0747-min-cost-climbing-stairs/solution.py
+++ 0747-min-cost-climbing-stairs/solution.py
@@ -1,12 +1,12 @@
-class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
-        firstStep  = 0
-        secondStep = 0
-        for i in xrange(2, len(cost)+1):
-            firstStep, secondStep = min(cost[i-1] + firstStep, cost[i-2] + secondStep), firstStep 
-            
-        return firstStep
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = {}
+        def dfs(index):
+            if index <= 0:
+                return 0
+            if index in memo:
+                return memo[index]
+            memo[index] = min(dfs(index-1), dfs(index - 2)) + cost[index-1]
+            return memo[index]
+
+        return min(dfs(len(cost)), dfs(len(cost) - 1))
