517f035cb3a0b004d76ac89bef478a99d390b992
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2024-12-30 21:31:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (32.95%)

--- 1086-divisor-game/solution.cpp
+++ 1086-divisor-game/solution.cpp
@@ -1,49 +1,8 @@
 class Solution {
+private:
+    unordered_map<int, bool> memo;
 public:
-    //remeber this
-    vector<int>result{vector<int>(10000, -1)};
-    
-    bool divisorGame(int N) 
-    {
-        result[0] = 0;
-        
-        if(N == 1)
-            return false;
-        
-        for(int i = 1;i <= N/2; i++)
-        {
-            if(N%i == 0)
-            {
-                if(result[N - i - 1] == -1)
-                {
-                    if(!divisorGame(N - i))
-                    {
-                        result[N - 1] = 1;
-                        return true;
-                    }
-                    else
-                    {
-                        result[N - 1] = 0;
-                        return false;
-                    }
-                }
-                else
-                {
-                    if(result[N-i-1] == 0)
-                    {
-                        result[N -1] = 1;
-                        return true;
-                    }
-                    else
-                    {
-                        result[N - 1] = 0;
-                        return false;
-                    }
-                }
-            }
-        }
-        
-        result[N - 1] = 0;
-        return false;
+    bool divisorGame(int n) {
+        return (n & 1) == 0;
     }
 };
