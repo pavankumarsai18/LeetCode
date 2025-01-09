c99fa4d77af38d7835ccc1d1427cd6b3acfc46c4
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2025-01-09 02:35:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.9 MB (99.46%)

--- 2292-counting-words-with-a-given-prefix/solution.cpp
+++ 2292-counting-words-with-a-given-prefix/solution.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int prefixCount(vector<string>& words, string pref) 
-    {
-        int numPrefix = 0;
-        const int prefixSize = pref.size();
-        for(int indx = 0; indx < words.size(); indx++)
-        {
-    
-    if(words[indx].size() >= prefixSize && words[indx].substr(0,prefixSize) == pref)
-            {
-                numPrefix++;
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0;
+        const int pref_len = pref.size();
+        for (int i = 0; i < words.size(); i++) {
+            string & word = words[i];
+            if (word.size() < pref_len) {
+                continue;
+            }
+            bool is_prefix = true;
+            for (int idx = 0; idx < pref_len; ++idx) {
+                if (pref[idx] != word[idx]) {
+                    is_prefix = false;
+                    break;
+                }
+            }
+
+            if (is_prefix) {
+                ans++;
             }
         }
-        
-        return numPrefix;
-        
+
+        return ans;
     }
 };
