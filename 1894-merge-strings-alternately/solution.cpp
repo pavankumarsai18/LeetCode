676d94fb3154bb13257356b2bddb681606d1c587
pavankumarsai18 <45509728+pavankumarsai18@users.noreply.github.com>
2025-05-19 17:46:35 +0000

    [LeetCode Sync] Runtime - 4 ms (31.96%), Memory - 8.4 MB (83.10%)

--- 1894-merge-strings-alternately/solution.cpp
+++ 1894-merge-strings-alternately/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        int i = 0;
         string ans = "";
 
-        for(int i = 0; i < min(word1.size(),word2.size()); ++i) {
-            ans += word1[i];
-            ans += word2[i];
-        }
-
-        if (word1.size() < word2.size()) {
-            ans += word2.substr(word1.size());
-        } else if (word2.size() < word1.size()) {
-            ans += word1.substr(word2.size());
+        while( i < word1.size() || i < word2.size()) {
+            if (i < word1.size()) ans += word1[i];
+            if (i < word2.size()) ans += word2[i];
+            i++;
         }
 
         return ans;
+        
     }
 };
